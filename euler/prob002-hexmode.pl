d003e86dd854731143191b7e9d90b7f7002dfa63
Mark A. Hershberger <mah@dococt.(none)>
2009-01-02 09:44:49 +0800

    solution to Euler problem #2 -- rakudo bug drove me crazy
    Signed-off-by: Eric Hodges <eric.hodges@gmail.com>

---
+++ euler/prob002-hexmode.pl
@@ -0,0 +1,26 @@
+#!perl6
+
+# code exposes a bug in r34733 rakudo, but works in pugs
+
+class Fibonacci {
+  has @!list is rw = (0, 1);
+
+  method next() {
+    @!list[2] = [+] @!list;
+    shift @!list;
+    return @!list[1];
+  }
+}
+
+my $fibber = Fibonacci.new;
+my $f;
+
+my @r = gather {
+  $f = $fibber.next;
+  while($f < 4000000) {
+    take $f if $f % 2 == 0;
+    $f = $fibber.next;
+  }
+}
+say @r.join(",");
+say [+] @r;
