ed5f8c51dcbd17cd725b262e4b5f8eec702c90a3
Nelo Onyiah <nelo.onyiah@gmail.com>
2009-01-05 04:56:38 +0800

    Added some usage documentation.
    Signed-off-by: Eric Hodges <eric.hodges@gmail.com>

--- wsg/beginner-2008/event001-j1n3l0.pl
+++ wsg/beginner-2008/event001-j1n3l0.pl
@@ -9,3 +9,19 @@ sub MAIN(*@cards) {
   } until @cards.elems == 0;
   say 'Total: ' ~ $pairs;
 }
+
+=begin pod
+
+=head1 NAME
+
+event001-j1n3l0.pl
+
+=head1 USAGE
+
+perl6 event001-j1n3l0.pl <cards>
+
+=head1 NOTE
+
+<cards> should be one of 2 .. 10, J, Q, K, A (though I don't check).
+
+=end pod
