701b2a76437a89f55e593d102de70a2125645859
Mark A. Hershberger <mah@dococt.(none)>
2009-02-16 04:34:07 +0800

    solution to euler 3
    Signed-off-by: Eric Hodges <eric.hodges@gmail.com>

---
+++ euler/prob003-hexmode.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+
+class Prime {
+  has @!primes is rw;
+  has Int $!number is rw = 1;
+
+  method next() {
+    my $not_prime = 1;
+
+    while($not_prime && $!number++) {
+      $not_prime = @!primes.grep({$!number % $^a == 0});
+    }
+    @!primes.push($!number);
+
+    my $copy = $!number;
+    return $copy;
+  }
+}
+
+my $p = Prime.new;
+my @q = gather { take $p.next for (0..10) };
+my @r;
+
+for (0..10) {
+  push @r, $p.next;
+}
+say @q.join(",");
+say @r.join(",");
