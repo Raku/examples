adf9e30d204a307db190ad35d1a2ee75056bb6d1
Moritz Lenz <moritz@faui2k3.org>
2009-03-01 19:18:36 +0800

    added even simpler perlmonks solution
    Signed-off-by: Eric Hodges <eric.hodges@gmail.com>

--- perlmonks/combinations-731808.pl
+++ perlmonks/combinations-731808.pl
@@ -3,8 +3,12 @@ use v6;
 my @urls = ('http://www.something.com/blah.aspx?code=',
             'http://www.somethingelse.com/stuff.aspx?thing=');
 
-my @ids = ('375035304','564564774','346464646');
+my @ids = <375035304 564564774 346464646>;
 
 my @combined = (@urls X @ids).map: {$^a ~ $^b};
 
 .say for @combined;
+
+# or even easier with the cross-meta operator:
+
+# .say for @urls X~ @ids;
