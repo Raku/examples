8d73f5df646c8fc590776c0cde74508fe4d3732c
dcarrera <daniel.carrera@theingots.org>
2009-06-01 11:43:06 +0200

    Add a counter at the end of the file name.

--- module-management/Fletcher.pl
+++ module-management/Fletcher.pl
@@ -1,4 +1,20 @@
-# Example: Foo::Bar-auth:92de-ver:1.2.0
+# A sketch program to compute unique file names for Perl modules based
+# on the Fletcher-16 hash.
+# 
+# Author: Daniel Carrera
+# License: Public Domain
+# 
+# Example: Foo::Bar-auth:92de-ver:1.2.0--0
+# 
+# Explanation:
+# 		The module name (Foo::Bar) is URL-encoded. After that, we add all
+#		metadata sorted by key. Version numbers are left intact. Other
+#		metadata is hashed with Fletcher-16.
+# 
+# 		Lastly, a counter is added at the end. If two different modules
+# 		get the exact same name (extremely unlikely) we use a counter to
+#		distinguish them.
+
 my @modules = (
 	{name => "Foo::Bar", meta => {auth=>'mailto:dave@example.com', ver=>'1.2.0'}},
 	{name => "Foo::動", meta => {auth=>'mailto:chang@example.com', ver=>'2.1.3'}}
@@ -13,7 +29,7 @@ sub filename($module,%meta) {
 		my $v = .value ~~ m/[\D & <-[.]>]/ ?? fletcher16(.value) !! .value;
 		$filename ~= "-" ~ .key ~ ":$v";
 	}
-	return $filename;
+	return $filename ~ "--0";
 }
 sub strencode($str) {
 	return $str.subst(/(<-alpha -[_:]>)/,{ charencode($0) },:g);
