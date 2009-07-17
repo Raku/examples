5b43d3c7d30d8d19f28e8daaaef88706fcb74362
Jonathan Scott Duff <duff@pobox.com>
2009-07-16 22:20:28 -0500

    A very brute force approach to Euler #53

---
+++ euler/prob053-duff.pl
@@ -0,0 +1,15 @@
+use v6;
+
+# brute force
+
+sub postfix:<!>($n) is cached { return [*] 1..$n }
+
+sub infix:<C>($n,$r)  { $n! / ($r! * ($n-$r)!); }
+
+my $count = 0;
+for 1..100 -> $n {
+    for 1..$n -> $r {
+        $count++  if $n C $r > 1_000_000;
+    }
+}
+say $count;
