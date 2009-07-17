b39437cb69c56978f1620f54d758e2fc0dc3309c
Jonathan Scott Duff <duff@pobox.com>
2009-07-16 21:45:58 -0500

    Add an optimization to Euler #52

--- euler/prob052-duff.pl
+++ euler/prob052-duff.pl
@@ -1,7 +1,11 @@
 use v6;
 
+
+sub log10($n) { return log($n) / log(10) }
+
 my $s = '';
-my $x = 1;
+my $mag = 1;
+my $x = $mag;
 loop {
     my $n = 2 * $x; my $s = $n.comb.sort;
     last if 
@@ -10,6 +14,10 @@ loop {
         $s eq (5*$x).comb.sort && 
         $s eq (6*$x).comb.sort;
     $x++;
+    if log10(6*$x).int > log10(2*$x).int {
+        $mag *= 10;
+        $x = $mag;
+    }
 }
 
 say $x;
