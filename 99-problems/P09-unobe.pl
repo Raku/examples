234e280756d9ebcf6179f9075e7b44d273d2cbec
David Romano <unobe@cpan.org>
2009-12-25 02:38:49 -0800

    * Use gather and loop for 99-problems/P09 new file:   P09-unobe.pl

---
+++ 99-problems/P09-unobe.pl
@@ -0,0 +1,19 @@
+#Pack consecutive duplicates of list elements into sublists.
+#If a list contains repeated elements they should be placed in separate sublists.
+#
+#Example:
+#* (pack '(a a a a b c c a a d e e e e))
+# ((A A A A) (B) (C C) (A A) (D) (E E E E))
+
+use v6;
+my @l = <a a a a b c c a a d e e e e>;
+sub prob09 (@in) {
+    return gather loop {
+        last if !@in.elems;
+        my $val = @in[0];
+    	take [gather { loop { @in[0] ~~ $val ?? take shift @in !! last }; }];
+    };
+}
+say ~@l;
+say prob09(@l).perl;
+
