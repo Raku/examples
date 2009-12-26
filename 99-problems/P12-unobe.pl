a96376cf3b65e7224e6b9d2ac7a26fe527992416
David Romano <unobe@cpan.org>
2009-12-25 22:23:54 -0800

    * Add 99-problems/P12 (oh how I wish xx were working!) new file:   P12-unobe.pl

---
+++ 99-problems/P12-unobe.pl
@@ -0,0 +1,27 @@
+# Given a run-length code list generated as specified in problem P11. Construct
+# its uncompressed version. 
+#
+#Example:
+#* (unpack '((4 a) b (2 c) (2 a) d (4 e)))
+#(a a a a b c c a a d e e e e)
+
+use v6;
+my @l = ([<4 a>],'b',[<2 c>],[<2 a>], 'd', [<4 e>]);
+sub prob12 (@in) {
+    my @out;
+    # XXX: gather { take @in[0].WHAT eq 'Array()' ?? { my @a = shift @in; @a[1] xx @a[0] } !! shift @in }
+    for 0 ... (@in.end) -> $x {
+        if @in[$x].WHAT eq 'Array()' {
+            loop (my $i=0; $i < @in[$x][0]; $i++) {
+                push @out, @in[$x][1];
+            };
+        }
+        else {
+            push @out, @in[$x];
+        }
+    }
+    return @out;
+}
+
+say @l.perl;
+say prob12(@l).perl;
