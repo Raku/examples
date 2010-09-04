a2f939b7e22567b2dac5cbea4e62c4e0c42a8d1b
Philip Potter <philip.g.potter@gmail.com>
2010-09-04 22:17:35 +0100

    add "our" to enable P08-viklund to compile under rakudo

--- 99-problems/P08-viklund.pl
+++ 99-problems/P08-viklund.pl
@@ -1,7 +1,9 @@
 use v6;
 
-multi infix:<compress> ( $a, $b ) { $a      ~~ $b ?? $a !! [ $a, $b ] }
-multi infix:<compress> ( @a, $b ) { @a[*-1] ~~ $b ?? @a !! [ @a, $b ] }
+# "our" is needed for the current rakudo (2010-09-04)
+# but as soon as it's not needed, it's better without
+our multi infix:<compress> ( $a, $b ) { $a      ~~ $b ?? $a !! [ $a, $b ] }
+our multi infix:<compress> ( @a, $b ) { @a[*-1] ~~ $b ?? @a !! [ @a, $b ] }
 
 say ([compress] <a a a a b c c a a d e e e e>).perl;
 
