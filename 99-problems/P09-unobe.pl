37bf29c0966e6b222f2642db25b1847f1f870ce6
Philip Potter <philip.g.potter@gmail.com>
2010-09-04 22:39:12 +0100

    change loop {} to while{} and remove last statements

--- 99-problems/P09-unobe.pl
+++ 99-problems/P09-unobe.pl
@@ -8,11 +8,10 @@
 use v6;
 my @l = <a a a a b c c a a d e e e e>;
 sub prob09 (@in) {
-    return gather loop {
-        last if !@in.elems;
+    return gather while @in.elems {
         my $val = @in[0];
-    	take [gather { loop { @in[0] ~~ $val ?? take shift @in !! last }; }];
-    };
+    	take [gather while @in[0] ~~ $val { take shift @in }];
+    }
 }
 say ~@l;
 say prob09(@l).perl;
