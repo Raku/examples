fb11fb020f5bac4531c5603c9a9b1b848b622165
Philip Potter <philip.g.potter@gmail.com>
2010-09-04 20:49:45 +0100

    P02 should return the last two elements, not just the second-last element

--- 99-problems/P02-scottp.pl
+++ 99-problems/P02-scottp.pl
@@ -3,18 +3,18 @@ use v6;
 
 # a. One line example
 #	<> can be used to generate an array, similar to perl 5 - qw<a b c d>
-#	[] is used to select the element number
+#	[a,b] is used to select a range of elements
 #	* means the number of elements
 #	say is like print to stdout with a new line
 #	.say can be called as everything is an object
-<A B C D E F>[* - 2].say;
+<A B C D E F>[*-2,*-1].say;
 
 # b. Subroutine example
 #	@l lists can be passed in as parameters - no need to use references
 #	.elems - is the number of elements, this time called on the object
 #	say called in procedure form
 sub my_but_last (@l) {
-	return @l[@l.elems - 2];
+	return @l[@l.elems-2,@l.elems-1];
 }
 say my_but_last(<a b c d>);
 
