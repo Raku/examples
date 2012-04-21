e6f1005ce3f07613885d6bcacb146622e3320818
Salve J. Nilsen <sjn+gnurf@kaizendo.org>
2012-04-21 21:14:27 +0200

    Update pot and add v6 requirements

--- best-of-rosettacode/100-doors.pl
+++ best-of-rosettacode/100-doors.pl
@@ -1,17 +1,35 @@
-# http://rosettacode.org/wiki/100_doors#Perl_6
+=begin pod
 
-# Problem: You have 100 doors in a row that are all initially closed. You make
-# 100 passes by the doors. The first time through, you visit every door and
-# toggle the door (if the door is closed, you open it; if it is open, you close
-# it). The second time you only visit every 2nd door (door #2, #4, #6, ...).
-# The third time, every 3rd door (door #3, #6, #9, ...), etc, until you only
-# visit the 100th door.
+=head1 Problem
 
-# Question: What state are the doors in after the last pass? Which are open,
-# which are closed?
+You have 100 doors in a row that are all initially closed. You make 100 passes
+by the doors. The first time through, you visit every door and toggle the door
+(if the door is closed, you open it; if it is open, you close it). The second
+time you only visit every 2nd door (door #2, #4, #6, ...).  The third time,
+every 3rd door (door #3, #6, #9, ...), etc, until you only visit the 100th
+door.
+
+=head1 Question
+
+What state are the doors in after the last pass? Which are open,
+which are closed?
+
+=head1 More
+
+L<http://rosettacode.org/wiki/100_doors#Perl_6>
+
+=end pod
+
 
 say "Door $_ is open" for 1..10 X** 2;
 
-# More about X** - http://perlcabal.org/syn/S03.html#Cross_operators
 
-# vim: expandtab shiftwidth=4 ft=perl6:
+=begin pod
+
+=head1 Features used
+
+C<X**> - http://perlcabal.org/syn/S03.html#Cross_operators
+
+=end pod
+
+# vim: expandtab shiftwidth=2 ft=perl6:
