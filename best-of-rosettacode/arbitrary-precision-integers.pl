a835aa2e5b6319340786627ab56f4f24813a10ea
Salve J. Nilsen <sjn+gnurf@kaizendo.org>
2012-04-22 11:23:25 +0200

    Add initial versions of two more examples

---
+++ best-of-rosettacode/arbitrary-precision-integers.pl
@@ -0,0 +1,40 @@
+use v6;
+
+=begin pod
+
+=head1 Arbitrary-precision integers (included)
+
+Using the in-built capabilities of your language, calculate the integer value of:
+
+        5^{4^{3^2}} 
+
+Confirm that the first and last twenty digits of the answer are: 62060698786608744707...92256259918212890625
+Find and show the number of decimal digits in the answer. 
+
+=head1 More
+
+L<http://rosettacode.org/wiki/Arbitrary-precision_integers_(included)#Perl_6>
+
+
+=head1 What's interesting here?
+
+=item metaoperator
+=item casting
+=item {} in string
+
+=end pod
+
+
+my $x = ~[**] 5, 4, 3, 2;
+say "5**4**3**2 = {substr($x,0,20)}...{substr($x,$x.chars-20)} and has {$x.chars} digits";
+
+
+=begin pod
+
+=head1 Features used
+
+=item C<> - L<http://perlcabal.org/syn/>
+
+=end pod
+
+# vim: expandtab shiftwidth=2 ft=perl6:
