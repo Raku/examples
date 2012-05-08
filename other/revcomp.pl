dd8202af2d0496f4fa783d212a1f9fac61f40c31
Gerhard R <gerd.r.devel@googlemail.com>
2012-05-08 13:33:49 +0200

    [other/revcomp.pl] fix whitespace

--- other/revcomp.pl
+++ other/revcomp.pl
@@ -1,22 +1,22 @@
 use v6;
 
-my %trans = 'wsatugcyrkmbdhvnATUGCYRKMBDHVN'.comb  Z=>
-	'WSTAACGRYMKVHDBNTAACGRYMKVHDBN'.comb;
+my %trans = 'wsatugcyrkmbdhvnATUGCYRKMBDHVN'.comb Z=>
+    'WSTAACGRYMKVHDBNTAACGRYMKVHDBN'.comb;
 
 my ($desc, @seq);
 
 for lines() {
-	dump, $desc = $_, next if /^ \>/;
-	@seq.unshift(.comb.reverse.map({ %trans{$^c} // $^c }));
-	LAST dump;
+    dump, $desc = $_, next if /^ \>/;
+    @seq.unshift(.comb.reverse.map({ %trans{$^c} // $^c }));
+    LAST dump;
 }
 
 sub dump {
-	LEAVE @seq = Nil;
-	return unless $desc.defined;
+    LEAVE @seq = Nil;
+    return unless $desc.defined;
 
-	@seq.splice($_, 0, "\n")
-		for (60, 120 ...^ * >= +@seq).reverse;
+    @seq.splice($_, 0, "\n")
+        for (60, 120 ...^ * >= +@seq).reverse;
 
-	say $desc, "\n", @seq.join;
+    say $desc, "\n", @seq.join;
 }
