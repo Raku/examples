0403454ed5950795f7fbc6647160a38d09caa271
Gerhard R <gerd.r.devel@googlemail.com>
2012-10-22 23:28:37 +0200

    [euler/prob003] add version using .is-prime
    align code of existing one to new version

--- euler/prob003-gerdr-cheating.pl
+++ euler/prob003-gerdr-cheating.pl
@@ -1,11 +1,9 @@
 use v6;
 
 sub largest-prime-factor($n is copy) {
-	my @primes;
 	for 2, 3, *+2 ... * {
-		next if $_ %% any @primes.grep(->$p { $p * $p <= $_ or last });
-		@primes.push($_);
-		while $n %% $_ {
+		next unless $n %% $_ and .is-prime;
+		repeat while $n %% $_ {
 			$n div= $_
 		}
 		return $_ if $_ > $n;
