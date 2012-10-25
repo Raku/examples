dc602bd93d8bdf7d104d07fb42a65a15ee3d731c
Gerhard R <gerd.r.devel@googlemail.com>
2012-10-25 13:25:40 +0200

    [euler/prob029] bikeshedding and benchmarking

--- euler/prob029-gerdr.pl
+++ euler/prob029-gerdr.pl
@@ -1,10 +1,14 @@
 use v6;
 
-# same algorithm as polettix' solution
+# compute number of unique powers a**b with bases \a in range 2..A
+# and exponents \b in range 2..B
 
-sub count-unique-powers(Int \A, Int \B --> Int) {
-	# computes number of unique powers a**b with bases \a in range 2..A
-	# and exponents \b in range 2..B
+sub count-naively(Int \A, Int \B --> Int) {
+	+(2..A X=> 2..B).classify({ .key ** .value })
+}
+
+sub count-smartly(Int \A, Int \B --> Int) {
+	# uses the same algorithm as polettix' solution
 
 	my %seen-bases;
 	my $seen-values = [+] gather {
@@ -21,7 +25,7 @@ sub count-unique-powers(Int \A, Int \B --> Int) {
 
 				# mark powers of \base according to their exponent
 				# relative to \root
-				%seen-exponents{(2..B).map(* * exp)} = True...*;
+				%seen-exponents{(2..B) >>*>> exp} = True xx *;
 
 				# avoid double-counting
 				%seen-bases{base} = True;
@@ -36,11 +40,17 @@ sub count-unique-powers(Int \A, Int \B --> Int) {
 	(A - 1 - %seen-bases) * (B - 1) + $seen-values
 }
 
-sub MAIN(Int $A = 100, Int $B = 100, Bool :$verify) {
-	say 'got ',
-		count-unique-powers $A, $B;
+sub bench(|) {
+	my \start = now;
+	my \result = callsame;
+	my \end = now;
+	return result, round (end - start) * 1000;
+}
 
-	say 'expected ',
-		+(2..$A X=> 2..$B).classify({ .key ** .value })
-		if $verify;
+sub MAIN(Int $A = 100, Int $B = 100, Bool :$verify) {
+	&count-smartly.wrap(&bench);
+	&count-naively.wrap(&bench);
+
+	printf "got %u [%ums]\n", count-smartly $A, $B;
+	printf "expected %u [%ums]\n", count-naively $A, $B if $verify;
 }
