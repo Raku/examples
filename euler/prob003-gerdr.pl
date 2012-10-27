294bc616f21bcd1b1c3d1a631727582d87d641a2
Gerhard R <gerd.r.devel@googlemail.com>
2012-10-27 21:01:40 +0200

    [euler/prob003] simplify algorithm
    composite numbers won't divide the reduced $n as their factors
    have already been seen and divided out

--- euler/prob003-gerdr.pl
+++ euler/prob003-gerdr.pl
@@ -1,14 +1,11 @@
 use v6;
 
 sub largest-prime-factor($n is copy) {
-	my @primes;
 	for 2, 3, *+2 ... * {
-		next if $_ %% any @primes.grep(->$p { $p * $p <= $_ or last });
-		@primes.push($_);
 		while $n %% $_ {
-			$n div= $_
+			$n div= $_;
+			return $_ if $_ > $n;
 		}
-		return $_ if $_ > $n;
 	}
 }
 
