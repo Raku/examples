ad9ae1705ea4dd03c680084254bddab5f278ea70
grondilu <grondilu@yahoo.fr>
2013-04-03 02:17:58 +0400

    Update cstr-grondilu.pl
    much faster version

--- rosalind/cstr-grondilu.pl
+++ rosalind/cstr-grondilu.pl
@@ -1,11 +1,11 @@
-my @dna = lines;
+my @dna = map { [.comb] }, lines;
 
-my @c = @dna[0].comb;
+my @c = @dna[0][];
 
 for ^@c -> $c {
     my $count = 0;
-    my @line = map { $count += my $x = +so .comb[$c] eq @c[$c]; $x }, @dna;
+    my @line = map { $count += my $x = +so .[$c] eq @c[$c]; $x }, @dna;
     say @line.join if 1 < $count < @dna-1;
 }
-
+     
 # vim: ft=perl6
