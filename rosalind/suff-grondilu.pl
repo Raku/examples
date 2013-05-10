22ef36310244e371d1ceb18e1890b3da1a484e85
L. Grondin <grondilu@yahoo.fr>
2013-05-10 18:49:54 +0200

    minor style rewrite

--- rosalind/suff-grondilu.pl
+++ rosalind/suff-grondilu.pl
@@ -1,34 +1,29 @@
 use v6;
 
-my $dna = get;
 sub suffixes(Str $str) { map *.flip, [\~] $str.flip.comb }
 sub suffix-tree(@a) {
-    if (@a == 0) { [] }
-    elsif (@a == 1) { return hash @a[0] => [] }
-    else {
-	return hash gather for @a.classify(*.substr(0, 1)) {
-	    my $subtree = suffix-tree([ grep *.chars, map *.substr(1), .value[] ]);
-	    if $subtree.elems == 1 {
-		my $pair = $subtree.pick;
-		take .key ~ $pair.key => $pair.value;
-	    } else {
-		take .key => $subtree;
-	    }
-	}
+    @a == 0 ?? [] !!
+    @a == 1 ?? hash @a[0] => [] !!
+    hash gather for @a.classify(*.substr(0, 1)) {
+        my $subtree = suffix-tree([ grep *.chars, map *.substr(1), .value[] ]);
+        if $subtree.elems == 1 {
+            my $pair = $subtree.pick;
+            take .key ~ $pair.key => $pair.value;
+        } else {
+            take .key => $subtree;
+        }
     }
 }
 
 sub show-edges($tree) {
     return if $tree.elems == 0;
     for $tree[] {
-	say .key;
-	show-edges .value;
+        say .key;
+        show-edges .value;
     }
 }
 
-my $tree = suffix-tree(suffixes($dna));
-
-show-edges $tree;
+show-edges suffix-tree suffixes get;
 
 
 # vim: ft=perl6
