52b5a129b4dfa8aad0a464d9c17a56829bf2c744
Aaron Sherman <ajs@cogolabs.com>
2013-12-02 11:48:24 -0800

    A second implementation that does half the work; more tests

--- 99-problems/P06-topo.pl
+++ 99-problems/P06-topo.pl
@@ -6,8 +6,35 @@ sub palindromic(@list)
     @list ~~ @list.reverse;
 }
 
-say palindromic(<a b c d e>.list);
-say palindromic(<a b c b a>.list);
+sub palindromic_short(@list) {
+    if @list.elems < 2 {
+	return True;
+    }
+    my $start = 0;
+    my $end = @list.elems - 1;
+    my $mid_start = floor(@list.elems / 2 - 1);
+    my $mid_end = ceiling(@list.elems / 2);
+    @list[$start .. $mid_start] ~~ @list[$mid_end .. $end].reverse;
+}
+
+my @functions = (&palindromic, &palindromic_short);
+my @examples = [
+    [[< a b c d E >], False],
+    [[< a b c b a >], True],
+    [[< a b b E >], False],
+    [[< E b b a>], False],
+    [[< a b b a >], True],
+    [[< a >], True],
+    [[< a a >], True],
+    [[< E a >], False] ].lol;
+
+for @examples -> ($list, $result) {
+    for @functions -> $f {
+	if $f($list) != $result {
+	    die "{$f}({$list.perl}) expect $result, but {$f($list).perl}";
+	}
+    }
+}
 
 =begin pod
 
