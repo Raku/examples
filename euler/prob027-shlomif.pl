83651e5286851d321b1874d357ca4bf2f9a285ff
Shlomi Fish <shlomif@shlomifish.org>
2014-01-13 18:02:19 +0200

    [Euler] Add #027.

---
+++ euler/prob027-shlomif.pl
@@ -0,0 +1,42 @@
+use v6;
+
+sub is_prime(Int $n) returns Bool
+{
+    if ($n <= 1)
+    {
+        return False;
+    }
+
+    for (2 .. $n.sqrt.floor) -> $i
+    {
+        if $n % $i == 0
+        {
+            return False;
+        }
+    }
+
+    return True;
+}
+
+my (Int $max_a, Int $max_b);
+
+my Int $max_iter = 0;
+for (0 .. 999) -> $b_coeff
+{
+    for ((-$b_coeff+1) .. 999) -> $a_coeff
+    {
+        my $n = 0;
+        while is_prime($b_coeff+$n*($n+$a_coeff))
+        {
+            $n++;
+        }
+        $n--;
+
+        if ($n > $max_iter)
+        {
+            ($max_a, $max_b, $max_iter) = ($a_coeff, $b_coeff, $n);
+        }
+    }
+}
+say "a = $max_a ; b = $max_b ; a*b = {$max_a*$max_b} ; n = $max_iter\n"
+
