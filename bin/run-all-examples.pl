75a0e1b62b1d6f5cfa2059436354369aef7e156d
Paul Cochrane <paul@liekut.de>
2015-02-22 14:00:51 +0100

    Add a script to run all examples

---
+++ bin/run-all-examples.pl
@@ -0,0 +1,45 @@
+use v6;
+
+=begin pod
+
+=head1 NAME
+
+run-all-examples.pl - run all current Perl 6 examples
+
+=head1 SYNOPSIS
+
+    $ perl6 run-all-examples.pl
+
+=head1 DESCRIPTION
+
+A script to run all current Perl 6 examples in the C<perl6-examples>
+repository and print their output.  This is useful to check if the examples
+compile and/or work as expected.
+
+=end pod
+
+my @example-dirs = qw{
+    99-problems
+    best-of-rosettacode
+    cookbook
+    euler
+    games
+    interpreters
+    module-management
+    parsers
+    perlmonks
+    rosalind
+    shootout
+    tutorial
+    wsg
+};
+
+for @example-dirs -> $dir {
+    my @example-files = dir($dir).sort.grep: /\.pl$/;
+    for @example-files -> $example {
+        say $dir ~ "/" ~ $example.basename;
+        qqx{perl6 $example}.say;
+    }
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6
