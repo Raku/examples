54670d4e055719044c3fb5e06c6deaaf5bd2045e
Paul Cochrane <paul@liekut.de>
2015-02-23 23:12:05 +0100

    Correct sigils; prob004-unobe.pl works again

--- euler/prob004-unobe.pl
+++ euler/prob004-unobe.pl
@@ -2,14 +2,14 @@ use v6;
 
 # another case where @array = %*% (100..999) would be nice to have:
 # http://use.perl.org/~dpuu/journal/38142
-sub diagonal_x ($array) {
-    my $result = [];
-    my $copy = $array;
-    for $copy -> $this {
-        $copy.shift;
-        for $copy -> $that { $result.push($this * $that); }
+sub diagonal_x (@array) {
+    my @result = [];
+    my @copy = @array;
+    for @copy -> $this {
+        @copy.shift;
+        for @copy -> $that { @result.push($this * $that); }
     }
-    return $result;
+    return @result;
 }
 
 diagonal_x(100..999).grep({ $_ eq .flip }).sort.reverse.[0].say;
