55ac1ae5e5313aa21bea9b0c00f470eb3b1bbab8
Paul Cochrane <paul@liekut.de>
2015-02-24 10:41:29 +0100

    Add sigils to prob009-gerdr-feeds.pl
    Now this example works again and roughly as originally intended.  It turns
    out that feeds which are split over multiple lines need a trailing backslash
    so that they aren't interpreted as bogus expressions by the compiler.

--- euler/prob009-gerdr-feeds.pl
+++ euler/prob009-gerdr-feeds.pl
@@ -1,23 +1,28 @@
 use v6;
 
-constant N = 1000;
+constant $N = 1000;
 
-# possible range of a
-1..Int((1 - sqrt(0.5)) * N)
+my $result;
+1..Int((1 - sqrt(0.5)) * $N) \
 
 # compute numerator and denominator of closed expression for b
-==> map -> \a { [ a, N * (N - 2 * a), 2 * (N - a) ] }
+==> map -> $a { [ $a, $N * ($N - 2 * $a), 2 * ($N - $a) ] } \
 
 # check if closed expression yields an integer
-==> grep -> [ \a, \u, \v ] { u %% v }
+==> grep -> [ $a, $u, $v ] { $u %% $v } \
 
 # compute b and c
-==> map -> [ \a, \u, \v ] { my \b = u div v; [ a, b, N - a - b ] }
+==> map -> [ $a, $u, $v ] { my $b = $u div $v; [ $a, $b, $N - $a - $b ] } \
 
 # compute product
-==> map -> @triple { [*] @triple }
+==> map -> @triple { [*] @triple } \
+
+# ... to give the result.
+# We don't .say directly on the output yet, since Rakudo (2015.02) doesn't
+# yet handle this case of a feed into a .say
+==> $result;
 
 # print result
-==> say;
+$result.say;
 
 # vim: expandtab shiftwidth=4 ft=perl6
