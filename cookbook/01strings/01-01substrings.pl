c343aa5f0706f974d62fd2f26c88d55a3c7acb74
Paul Cochrane <paul@liekut.de>
2015-02-25 14:06:14 +0100

    Restore functionality in cookbook/01strings/01-01substrings.pl

--- cookbook/01strings/01-01substrings.pl
+++ cookbook/01strings/01-01substrings.pl
@@ -11,7 +11,12 @@ my ($string, $offset, $count) = ('Pugs is da bomb', 2, 5);
 say $string.substr($offset, $count);
 say $string.substr($offset);
 
-substr($string, $offset) = "gilism ain't for wimps";
+# want to replace everything but the first two letters with the string
+# "gilism ain't for wimps"
+# this code works in Perl 5, but not in Perl 6
+# substr($string, $offset) = "gilism ain't for wimps";
+# say $string;
+$string = $string.substr(0, $offset) ~ "gilism ain't for wimps";
 say $string;
 
 # vim: expandtab shiftwidth=4 ft=perl6
