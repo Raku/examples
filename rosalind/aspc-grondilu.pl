772078d26c3a086430d5b34630bdbd3826a1ec07
Paul Cochrane <paul@liekut.de>
2015-03-01 11:47:06 +0100

    Set up default values for rosalind problem aspc

--- rosalind/aspc-grondilu.pl
+++ rosalind/aspc-grondilu.pl
@@ -1,9 +1,5 @@
 use v6;
 
-my ($n, $m) = get.words;
-my $sum = my $C =  ([*] $n-$m+1 .. $n) div [*] 1 .. $m;
-for $m+1 .. $n -> $k { $sum += $C = $C * ($n - $k + 1) div $k }
-say $sum % 1_000_000;
 =begin pod
 
 Introduction to Alternative Splicing
@@ -15,5 +11,11 @@ Expected output from default input data:
     42
 
 =end pod
+
+sub MAIN($n = 6, $m = 3) {
+    my $sum = my $C =  ([*] $n-$m+1 .. $n) div [*] 1 .. $m;
+    for $m+1 .. $n -> $k { $sum += $C = $C * ($n - $k + 1) div $k }
+    say $sum % 1_000_000;
+}
 
 # vim: expandtab shiftwidth=4 ft=perl6
