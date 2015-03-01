606f851926898791fadcaee00f642ce95e790671
Paul Cochrane <paul@liekut.de>
2015-03-01 11:46:42 +0100

    Document rosalind problem aspc

--- rosalind/aspc-grondilu.pl
+++ rosalind/aspc-grondilu.pl
@@ -4,5 +4,16 @@ my ($n, $m) = get.words;
 my $sum = my $C =  ([*] $n-$m+1 .. $n) div [*] 1 .. $m;
 for $m+1 .. $n -> $k { $sum += $C = $C * ($n - $k + 1) div $k }
 say $sum % 1_000_000;
+=begin pod
+
+Introduction to Alternative Splicing
+
+http://rosalind.info/problems/aspc/
+
+Expected output from default input data:
+
+    42
+
+=end pod
 
 # vim: expandtab shiftwidth=4 ft=perl6
