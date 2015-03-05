e7eaadc14cb3c5e2f46f0fffdf547fe27cb91e35
Paul Cochrane <paul@liekut.de>
2015-03-05 17:40:44 +0100

    [rosalind] match output format of website for indc example

--- categories/rosalind/indc-grondilu.pl
+++ categories/rosalind/indc-grondilu.pl
@@ -24,8 +24,10 @@ sub C($n, $k) {
         $k == 1 ?? $n !! C($n, $k-1)*($n-$k+1) div $k;
 }
 
-say gather for 1 .. 2*$n -> $k {
+my @results = gather for 1 .. 2*$n -> $k {
     take .log / log 10 given 1/2**(2*$n) * [+] map { C 2*$n, $_ }, $k .. 2*$n;
 }
+
+say @results.fmt("%.3f");
 
 # vim: expandtab shiftwidth=4 ft=perl6
