1760ea767fe142bd2aa47a5e9cf30681f96a82ea
Paul Cochrane <paul@liekut.de>
2015-03-05 19:53:50 +0100

    [rosalind] add initial docs for dna example

--- categories/rosalind/dna-gerdr.pl
+++ categories/rosalind/dna-gerdr.pl
@@ -1,5 +1,21 @@
 use v6;
 
 <A C G T>.map({ +.comb(/$^symbol/) }).say given slurp;
+=begin pod
+
+=TITLE Counting DNA Nucleotides
+
+L<http://rosalind.info/problems/dna/>
+
+Sample input
+
+    AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
+
+Sample output
+
+    20 12 17 21
+
+=end pod
+
 
 # vim: expandtab shiftwidth=4 ft=perl6
