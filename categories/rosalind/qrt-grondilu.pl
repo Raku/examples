f50f25e376c44763c2f3af8cd93702d4716e8704
Paul Cochrane <paul@liekut.de>
2015-03-05 22:07:21 +0100

    [rosalind] handle default and cli input in qrt example

--- categories/rosalind/qrt-grondilu.pl
+++ categories/rosalind/qrt-grondilu.pl
@@ -38,23 +38,33 @@ sub ncombine($n, $k) {
     (state@)[$n][$k] //= combine($k, [^$n])
 }
 
-my @data = lines;
+my $default-input = q:to/END/;
+    cat dog elephant ostrich mouse rabbit robot
+    01xxx00
+    x11xx00
+    111x00x
+    END
 
-my %seen;
-my @taxa = @data.shift.words;
-for @data -> $line {
-    my %h = @taxa Z=> $line.comb;
-    my @classif =
-    classify( { %h{$_} }, @taxa )<0 1>;
-    next if any(@classif) < 2;
-    note "processing $line";
-    my @a = map { [ @classif[0][@$_] ] }, ncombine(@classif[0].elems, 2)[];
-    my @b = map { [ @classif[1][@$_] ] }, ncombine(@classif[1].elems, 2)[];
-    for @a X @b -> $a, $b {
-        my $left = $a.sort.join(', ');
-        my $right = $b.sort.join(', ');
-        next if %seen{$left}{$right}++ ?| %seen{$right}{$left}++;
-        say '{' ~ $left ~ '} {' ~ $right ~ '}';
+sub MAIN($input-file = Nil) {
+    my $input = $input-file ?? $input-file.IO.slurp !! $default-input;
+    my @data = $input.lines;
+
+    my %seen;
+    my @taxa = @data.shift.words;
+    for @data -> $line {
+        my %h = @taxa Z=> $line.comb;
+        my @classif =
+        classify( { %h{$_} }, @taxa )<0 1>;
+        next if any(@classif) < 2;
+        note "processing $line";
+        my @a = map { [ @classif[0][@$_] ] }, ncombine(@classif[0].elems, 2)[];
+        my @b = map { [ @classif[1][@$_] ] }, ncombine(@classif[1].elems, 2)[];
+        for @a X @b -> $a, $b {
+            my $left = $a.sort.join(', ');
+            my $right = $b.sort.join(', ');
+            next if %seen{$left}{$right}++ ?| %seen{$right}{$left}++;
+            say '{' ~ $left ~ '} {' ~ $right ~ '}';
+        }
     }
 }
 
