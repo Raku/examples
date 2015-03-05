a910ba72bf894f0e7da65333a081af3ddfa2fa40
Paul Cochrane <paul@liekut.de>
2015-03-05 20:01:37 +0100

    [rosalind] add initial docs to other dna example

--- categories/rosalind/dna-grondilu.pl
+++ categories/rosalind/dna-grondilu.pl
@@ -1,5 +1,24 @@
 use v6;
 
+=begin pod
+
+=TITLE Counting DNA Nucleotides
+
+=AUTHOR grondilu
+
+L<http://rosalind.info/problems/dna/>
+
+Sample input
+
+    AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
+
+Sample output
+
+    20 12 17 21
+
+=end pod
+
+
 say get.comb.bag<A C G T>;
 
 # vim: expandtab shiftwidth=4 ft=perl6
