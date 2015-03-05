28e49c8dd7c71f5ccd327ea85618b75496f0ddd1
Paul Cochrane <paul@liekut.de>
2015-03-05 20:25:29 +0100

    [rosalind] add initial docs for fib example

--- categories/rosalind/fib-grondilu.pl
+++ categories/rosalind/fib-grondilu.pl
@@ -1,5 +1,23 @@
 use v6;
 
+=begin pod
+
+=TITLE Rabbits and Recurrence Relations
+
+=AUTHOR grondilu
+
+L<http://rosalind.info/problems/fib/>
+
+Sample input
+
+    5 3
+
+Sample output
+
+    19
+
+=end pod
+
 my ($n, $k) = get.words;
 my @fib := 1, 1, * * $k + * ... *;
 
