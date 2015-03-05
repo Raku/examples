34ed9af16b451fce2265e835a33e89ccf35e1773
Paul Cochrane <paul@liekut.de>
2015-03-05 19:01:21 +0100

    [rosalind] handle default and cmd line input in prob example

--- categories/rosalind/prob-grondilu.pl
+++ categories/rosalind/prob-grondilu.pl
@@ -20,15 +20,18 @@ Sample output
 my @sample = "ACGATACAA
 0.129 0.287 0.423 0.476 0.641 0.742 0.783".split: "\n";
 
-my $dna = $*IN.get;
-my @A = $*IN.get.split(" ");
+sub MAIN($input-file = Nil) {
+    my ($dna, $gc-content-string) = $input-file ?? $input-file.IO.lines !! @sample;
+    my @A = $gc-content-string.split(" ");
+    my @B = map { log(prob :$dna, :gc-content($_))/log(10) }, @A;
 
-sub prob(:$dna, :$gc-content) {
-    1/2**$dna.chars * [*] map { $_ eq 'G'|'C' ?? $gc-content !! (1 - $gc-content) }, $dna.comb
+    say @B».fmt: "%.3f";
 }
 
-my @B = map { log(prob :$dna, :gc-content($_))/log(10) }, @A;
-
-say @B».fmt: "%.3f";
+sub prob(:$dna, :$gc-content) {
+    1/2**$dna.chars * [*] map {
+        $_ eq 'G'|'C' ?? $gc-content !! (1 - $gc-content)
+    }, $dna.comb;
+}
 
 # vim: expandtab shiftwidth=4 ft=perl6
