69f37652ff680ad90765372bf5b8fd34a3bfe8bf
Paul Cochrane <paul@liekut.de>
2015-03-05 22:09:43 +0100

    [rosalind] add initial docs for revc example

--- categories/rosalind/revc-gerdr.pl
+++ categories/rosalind/revc-gerdr.pl
@@ -1,5 +1,23 @@
 use v6;
 
+=begin pod
+
+=TITLE Complementing a Strand of DNA
+
+=AUTHOR grondilu
+
+L<http://rosalind.info/problems/revc/>
+
+Sample input
+
+    AAAACCCGGT
+
+Sample output
+
+    ACCGGGTTTT
+
+=end pod
+
 .flip.trans('ACGT' => 'TGCA').say given slurp;
 
 # vim: expandtab shiftwidth=4 ft=perl6
