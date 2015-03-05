7e0faabaa38035c384bd225a71a4f064d9418c5d
Paul Cochrane <paul@liekut.de>
2015-03-05 19:44:18 +0100

    [rosalind] handle default and command line input for ctbl example

--- categories/rosalind/ctbl-grondilu.pl
+++ categories/rosalind/ctbl-grondilu.pl
@@ -1,22 +1,5 @@
 use v6;
 
-my $line = get; #"(dog,((elephant,mouse),robot),cat);";
-$line .=chop;
-my $names = rx / <.ident>+ | <?after <[(,]> > <?before <[),]> > /;
-my @name = $line.comb: $names;
-my @sorting = map *.value, sort *.key, (@name Z=> ^@name);
-$line ~~ s:g/$names/0/;
-$line ~~ s:g/','//;
-
-while $line ~~ / \( 0 ** 2..* \) / {
-    my $array = join(
-	'',
-	.prematch,
-	.subst(/0/, '1', :g),
-	.postmatch,
-    ).subst(/<[()]>/, '', :g) given $/;
-    unless $array ~~ /^[ 0+ | 1+ ]$/ {
-	say $array.comb[@sorting].join: '';
 =begin pod
 
 =TITLE Creating a Character Table
@@ -34,8 +17,27 @@ Sample output
 
 =end pod
 
+sub MAIN(Str $input = "(dog,((elephant,mouse),robot),cat);") {
+    my $line = $input;
+    $line .= chop;
+    my $names = rx / <.ident>+ | <?after <[(,]> > <?before <[),]> > /;
+    my @name = $line.comb: $names;
+    my @sorting = map *.value, sort *.key, (@name Z=> ^@name);
+    $line ~~ s:g/$names/0/;
+    $line ~~ s:g/','//;
+
+    while $line ~~ / \( 0 ** 2..* \) / {
+        my $array = join(
+            '',
+            .prematch,
+            .subst(/0/, '1', :g),
+            .postmatch,
+        ).subst(/<[()]>/, '', :g) given $/;
+        unless $array ~~ /^[ 0+ | 1+ ]$/ {
+            say $array.comb[@sorting].join: '';
+        }
+        $line ~~ s/\( (0+) \)/$0/;
     }
-    $line ~~ s/\( (0+) \)/$0/;
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
