c412098be902408d22ffc89c6c760da7b12a0a4f
Paul Cochrane <paul@liekut.de>
2015-03-05 19:36:50 +0100

    [rosalind] handle default and cli input data in cstr example

--- categories/rosalind/cstr-grondilu.pl
+++ categories/rosalind/cstr-grondilu.pl
@@ -21,13 +21,23 @@ Sample output
 
 =end pod
 
-my @dna = map { [.comb] }, lines;
+my @default-data = qw:to/END/;
+    ATGCTACC
+    CGTTTACC
+    ATTCGACC
+    AGTCTCCC
+    CGTCTATC
+    END
 
-my @c = @dna[0][];
+sub MAIN($input-file = Nil) {
+    my @input = $input-file ?? $input-file.IO.lines !! @default-data;
+    my @dna = map { [.comb] }, @input;
+    my @c = @dna[0][];
 
-for ^@c -> $c {
-    my @line = map { +so .[$c] eq @c[$c] }, @dna;
-    say @line.join if 1 < ([+] @line) < @dna-1;
+    for ^@c -> $c {
+        my @line = map { +so .[$c] eq @c[$c] }, @dna;
+        say @line.join if 1 < ([+] @line) < @dna-1;
+    }
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
