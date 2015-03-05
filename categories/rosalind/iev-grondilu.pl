627cfe6578e43ce222c687d73a8313e5b85bbe6f
Paul Cochrane <paul@liekut.de>
2015-03-05 17:20:07 +0100

    [rosalind] set default data for iev example

--- categories/rosalind/iev-grondilu.pl
+++ categories/rosalind/iev-grondilu.pl
@@ -1,5 +1,8 @@
 use v6;
 
-say 2 * [+] <1 1 1 3/4 1/2 0> Z*  $*IN.get.split: " ";
+
+sub MAIN(Str $default-data = "1 0 0 1 0 1") {
+    say 2 * [+] <1 1 1 3/4 1/2 0> Z* $default-data.split: " ";
+}
 
 # vim: expandtab shiftwidth=4 ft=perl6
