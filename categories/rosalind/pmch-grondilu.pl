62163bee5b3d51e6aedbaaa0287b6712f0f0f639
Paul Cochrane <paul@liekut.de>
2015-03-05 21:58:55 +0100

    [rosalind] add initial docs to pmch example

--- categories/rosalind/pmch-grondilu.pl
+++ categories/rosalind/pmch-grondilu.pl
@@ -1,5 +1,24 @@
 use v6;
 
+=begin pod
+
+=TITLE Perfect Matchings and RNA Secondary Structures
+
+=AUTHOR grondilu
+
+L<http://rosalind.info/problems/pmch/>
+
+Sample input
+
+    >Rosalind_23
+    AGCUAGUCAU
+
+Sample output
+
+    12
+
+=end pod
+
 my $rna = join '', grep /^ <[ACGU]>+ $/, lines;
 
 say [*] map { [*] 1 .. $rna.comb(/$_/).elems }, <U C>;
