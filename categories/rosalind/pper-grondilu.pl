5534366926ea7f67033fbe9460fa4611fde5263b
Paul Cochrane <paul@liekut.de>
2015-03-05 18:45:41 +0100

    [rosalind] add initial docs for pper example

--- categories/rosalind/pper-grondilu.pl
+++ categories/rosalind/pper-grondilu.pl
@@ -1,5 +1,21 @@
 use v6;
 
+=begin pod
+
+=TITLE Partial Permutations
+
+L<http://rosalind.info/problems/pper/>
+
+Sample input
+
+    21 7
+
+Sample output
+
+    51200
+
+=end pod
+
 sub pper($n, $k) { [*] $n-$k+1 .. $n }
 say pper(| $*IN.get.split: " ") % 1_000_000;
 
