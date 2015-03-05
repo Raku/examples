6ef3b61099bf56e71c73763243a8060998e47541
Paul Cochrane <paul@liekut.de>
2015-03-05 20:31:34 +0100

    [rosalind] add initial docs for sexl example

--- categories/rosalind/sexl-grondilu.pl
+++ categories/rosalind/sexl-grondilu.pl
@@ -1,5 +1,23 @@
 use v6;
 
+=begin pod
+
+=TITLE Sex-Linked Inheritance
+
+=AUTHOR grondilu
+
+L<http://rosalind.info/problems/sexl/>
+
+Sample input
+
+    0.1 0.5 0.8
+
+Sample output
+
+    0.18 0.5 0.32
+
+=end pod
+
 say map { 2*$^x*(1-$x) }, get.split(' ')».Num;
 
 # vim: expandtab shiftwidth=4 ft=perl6
