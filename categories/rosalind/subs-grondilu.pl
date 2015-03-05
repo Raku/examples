eb851e685ff785c20ff7ad27aec1af70e7d9a30d
Paul Cochrane <paul@liekut.de>
2015-03-05 19:29:44 +0100

    [rosalind] add initial docs to subs example

--- categories/rosalind/subs-grondilu.pl
+++ categories/rosalind/subs-grondilu.pl
@@ -1,5 +1,22 @@
 use v6;
 
+=begin pod
+
+=TITLE Finding a Motif in DNA
+
+L<http://rosalind.info/problems/subs/>
+
+Sample input
+
+    GATATATGCATATACTT
+    ATAT
+
+Sample output
+
+    2 4 10
+
+=end pod
+
 my ($S, $t) = $*IN.lines;
 say gather for $S.match(/$t/, :overlap) { take 1+.from }
 
