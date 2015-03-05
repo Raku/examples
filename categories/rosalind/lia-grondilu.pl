b0c55d7f40f027cf42be79027fe89f39b2157d59
Paul Cochrane <paul@liekut.de>
2015-03-05 17:53:24 +0100

    [rosalind] fix whitespace issues

--- categories/rosalind/lia-grondilu.pl
+++ categories/rosalind/lia-grondilu.pl
@@ -20,10 +20,10 @@ my ($k, $N) = $*IN.get.split(" ")».Int;
 
 my %p = <AA Aa aa> Z=> <0 1 0>;
 for ^$k {
-    %p = 
-    :AA(2 * [+] %p<AA Aa aa> Z* <1/2 1/4 0>),
-    :Aa(2 * [+] %p<AA Aa aa> Z* <1/2 1/2 1/2>),
-    :aa(2 * [+] %p<AA Aa aa> Z* <0 1/4 1/2>),
+    %p =
+        :AA(2 * [+] %p<AA Aa aa> Z* <1/2 1/4 0>),
+        :Aa(2 * [+] %p<AA Aa aa> Z* <1/2 1/2 1/2>),
+        :aa(2 * [+] %p<AA Aa aa> Z* <0 1/4 1/2>),
     ;
 }
 die unless 2**$k == [+] %p.values;
@@ -32,8 +32,7 @@ $p *= $p;
 
 sub postfix:<!>($a) { [*] 1..$a }
 sub binomialcoefficient($n, $k) {
-    $n! 
-    / (($n - $k)! * $k!);
+    $n! / (($n - $k)! * $k!);
 }
 my $total = 2**$k;
 say [+] map -> $i { (1-$p)**($total - $i)*$p**$i * binomialcoefficient( $total, $i ) }, $N .. $total;
