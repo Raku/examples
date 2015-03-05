5fc431aec568226254c63b9cb073f5dab614cc8b
Paul Cochrane <paul@liekut.de>
2015-03-05 22:12:22 +0100

    [rosalind] add initial docs for rna example

--- categories/rosalind/rna-gerdr.pl
+++ categories/rosalind/rna-gerdr.pl
@@ -1,5 +1,23 @@
 use v6;
 
+=begin pod
+
+=TITLE Transcribing DNA into RNA
+
+=AUTHOR gerdr
+
+L<http://rosalind.info/problems/rna/>
+
+Sample input
+
+    GATGGAACTTGACTACGTAAATT
+
+Sample output
+
+    GAUGGAACUUGACUACGUAAAUU
+
+=end pod
+
 .trans('T' => 'U').say given slurp;
 
 # vim: expandtab shiftwidth=4 ft=perl6
