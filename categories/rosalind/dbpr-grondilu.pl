b0005df0459428d40db34791ca5741184036f981
Paul Cochrane <paul@liekut.de>
2015-03-05 19:47:21 +0100

    [rosalind] add initial docs for dbpr example

--- categories/rosalind/dbpr-grondilu.pl
+++ categories/rosalind/dbpr-grondilu.pl
@@ -1,4 +1,23 @@
 use v6;
+
+=begin pod
+
+=TITLE Introduction to Protein Databases
+
+L<http://rosalind.info/problems/dbpr/>
+
+Sample input
+
+    Q5SLP9
+
+Sample output
+
+    DNA recombination
+    DNA repair
+    DNA replication
+
+=end pod
+
 use LWP::Simple;
 my $id = get;
 for split "\n", LWP::Simple.get(qq{http://www.uniprot.org/uniprot/$id.txt}) {
