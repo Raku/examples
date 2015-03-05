c9f78ecb2e05f34c11c81013ddcefec2d42226fd
Paul Cochrane <paul@liekut.de>
2015-03-05 20:48:41 +0100

    [rosalind] add initial docs for lcsq example

--- categories/rosalind/lcsq-grondilu.pl
+++ categories/rosalind/lcsq-grondilu.pl
@@ -1,5 +1,32 @@
 use v6;
 
+=begin pod
+
+=TITLE Finding a Shared Spliced Motif
+
+=AUTHOR grondilu
+
+L<http://rosalind.info/problems/lcsq/>
+
+Sample input
+
+    >Rosalind_23
+    AACCTTGG
+    >Rosalind_64
+    ACACTGTGA
+
+Sample output
+
+    AACTG
+
+Note: the C<lcsq> shared library needs to be built first.  For example, on a
+Linux system use the following commands:
+
+    $ gcc --std=c99 -fPIC -c -o lcsq.o lcsq.c
+    $ gcc -shared -o lcsq.so lcsq.o
+
+=end pod
+
 use NativeCall;
 sub lcsq(Str $, Str $ --> Str) is native('./lcsq') {*}
 
