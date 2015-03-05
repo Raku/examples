7dac4cb27d567a44dbb756cc2a77c7d186c41a8a
Paul Cochrane <paul@liekut.de>
2015-03-05 17:15:14 +0100

    [rosalind] set default data in hamm example

--- categories/rosalind/hamm-grondilu.pl
+++ categories/rosalind/hamm-grondilu.pl
@@ -1,6 +1,9 @@
 use v6;
 
-my ($S, $t) = $*IN.lines;
-say [+] ($S.comb Zeq $t.comb)».not».Int;
+
+sub MAIN(@default-data = qw{GAGCCTACTAACGGGAT CATCGTAATGACGGCCT}) {
+    my ($S, $t) = @default-data;
+    say [+] ($S.comb Zeq $t.comb)».not».Int;
+}
 
 # vim: expandtab shiftwidth=4 ft=perl6
