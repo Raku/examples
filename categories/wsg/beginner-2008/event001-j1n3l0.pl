fab2566b1d6dd325d32b2a102cd67966664b7993
Paul Cochrane <paul@liekut.de>
2015-03-06 10:54:23 +0100

    [wsg] use default set of cards
    Also initialise the $pairs variable.

--- categories/wsg/beginner-2008/event001-j1n3l0.pl
+++ categories/wsg/beginner-2008/event001-j1n3l0.pl
@@ -1,14 +1,4 @@
 use v6;
-our $pairs;
-sub MAIN(*@cards) {
-    repeat {
-        my $card = @cards.shift;
-        for @cards -> $other_card {
-            $pairs++ if $card eq $other_card;
-        }
-    } until @cards.elems == 0;
-    say 'Total: ' ~ $pairs;
-}
 
 =begin pod
 
@@ -27,5 +17,17 @@ sub MAIN(*@cards) {
 L<http://web.archive.org/web/20080324231941/http://www.microsoft.com/technet/scriptcenter/funzone/games/games08/bevent1.mspx>
 
 =end pod
+
+my @default-cards = qw{7 5 7 7 K};
+sub MAIN(@cards = @default-cards) {
+    my $pairs = 0;
+    repeat {
+        my $card = @cards.shift;
+        for @cards -> $other_card {
+            $pairs++ if $card eq $other_card;
+        }
+    } until @cards.elems == 0;
+    say 'Total: ' ~ $pairs;
+}
 
 # vim: expandtab shiftwidth=4 ft=perl6
