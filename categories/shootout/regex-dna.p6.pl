64f1495fba489aed780c8dbee921f367db73ac52
Paul Cochrane <paul@liekut.de>
2015-03-06 06:31:40 +0100

    [shootout] add initial docs for regex-dna example

--- categories/shootout/regex-dna.p6.pl
+++ categories/shootout/regex-dna.p6.pl
@@ -1,11 +1,18 @@
-# The Computer Language Benchmarks Game
-#
-# Based on the submission for Perl 5.
-# originally contributed by Daniel carrera
-#
-# USAGE: perl6 regex-dna.p6.pl < regex-dna.input
-
 use v6;
+
+=begin pod
+
+=TITLE Match DNA 8-mers and substitute nucleotides for IUB code
+
+=AUTHOR Daniel Carrera
+
+Based on the submission for Perl 5.
+
+L<http://benchmarksgame.alioth.debian.org/u32/performance.php?test=regexdna>
+
+USAGE: perl6 regex-dna.p6.pl regex-dna.input
+
+=end pod
 
 my $input = slurp;
 my $data = $input.lines.grep({ $_ !~~ /^ \>/}).join.lc;
@@ -31,3 +38,5 @@ my %iub = 'b' => '(c|g|t)', 'd' => '(a|g|t)', 'h' => '(a|c|t)',
 my $output = $data.subst(/(<[bdhkmnrsvwy]>)/, { %iub{$_} }, :g);
 
 .chars.say for $input, $data, $output;
+
+# vim: expandtab shiftwidth=4 ft=perl6
