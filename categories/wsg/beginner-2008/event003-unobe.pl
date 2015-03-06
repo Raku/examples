06fdee66523086bb6662ce464c84987180026959
Paul Cochrane <paul@liekut.de>
2015-03-06 11:24:28 +0100

    [wsg] use Rakudo's dir() to find files

--- categories/wsg/beginner-2008/event003-unobe.pl
+++ categories/wsg/beginner-2008/event003-unobe.pl
@@ -35,21 +35,15 @@ L<http://web.archive.org/web/20081227065437/http://www.microsoft.com/technet/scr
 
 =end pod
 
-# There is no IO::Dir::open implemented in Rakudo yet, and I don't know how
-# to make this output become an IO stream to implement it according to
-# spec...so just use PIR directly for now
-my @files = q:PIR {
-    $P1 = new ['OS']
-    $P2 = $P1.'readdir'('.') # change to whatever the script directory is
-    %r = 'list'($P2)
-};
-my $output = 'firstlines.out';
+my $run-dir = $*PROGRAM_NAME.IO.dirname;
+my @files = dir($run-dir);
+my $output = $run-dir ~ '/firstlines.out';
 
 # only select .txt files
 for @files.grep: { .match(/\.txt $$/) } {
     my $inputfh = open $_, :r;
     my $outputfh = open $output, :a;
-    $outputfh.say( $($inputfh.readline) ); # $(...) forces item context
+    $outputfh.say( $($inputfh.get) ); # $(...) forces item context
     .close for $inputfh, $outputfh;
 }
 
