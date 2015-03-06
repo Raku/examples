73fefa8dd25b37af04a404191683d9b52576659a
Paul Cochrane <paul@liekut.de>
2015-03-06 11:40:03 +0100

    [wsg] use prompt() instead of $*IN

--- categories/wsg/beginner-2008/event008-eric256.pl
+++ categories/wsg/beginner-2008/event008-eric256.pl
@@ -33,9 +33,8 @@ my $guess = 0;
 
 
 loop {
-    say "Pick a number between 1 and 50";
+    $guess = prompt("Pick a number between 1 and 50: ");
     $guesses++;
-    $guess = =$*IN;
     given $guess {
         when $num {
             say "You guessed correctly in {$guesses} guesses!";
