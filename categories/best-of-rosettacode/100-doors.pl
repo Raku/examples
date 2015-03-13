73832bbd345ec08632a3eeb0bda47f1a6a3a56e7
Paul Cochrane <paul@liekut.de>
2015-03-13 19:43:46 +0100

    [rosettacode] merge pod blocks; add AUTHOR

--- categories/best-of-rosettacode/100-doors.pl
+++ categories/best-of-rosettacode/100-doors.pl
@@ -4,6 +4,8 @@ use v6;
 
 =TITLE 100 Doors
 
+=AUTHOR Salve J. Nilsen
+
 You have 100 doors in a row that are all initially closed. You make 100 passes
 by the doors. The first time through, you visit every door and toggle the door
 (if the door is closed, you open it; if it is open, you close it). The second
@@ -20,18 +22,12 @@ which are closed?
 
 L<http://rosettacode.org/wiki/100_doors#Perl_6>
 
-=end pod
-
-
-say "Door $_ is open" for 1..10 X** 2;
-
-
-=begin pod
-
 =head1 Features used
 
 C<X**> - L<http://perlcabal.org/syn/S03.html#Cross_operators>
 
 =end pod
+
+say "Door $_ is open" for 1..10 X** 2;
 
 # vim: expandtab shiftwidth=4 ft=perl6
