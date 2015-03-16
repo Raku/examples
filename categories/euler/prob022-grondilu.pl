328646ab66be83241a33ba3085c1097574a42958
Paul Cochrane <paul@liekut.de>
2015-03-16 16:00:48 +0100

    [euler] read names.txt independently of run dir

--- categories/euler/prob022-grondilu.pl
+++ categories/euler/prob022-grondilu.pl
@@ -1,8 +1,9 @@
 use v6;
 my $i = 1;
 constant A = 'A'.ord - 1;
+my $names-file = $*PROGRAM_NAME.IO.dirname ~ "/names.txt";
 say [+] gather
-for sort slurp 'names.txt' {
+for sort slurp $names-file {
     take $i++ * [+] .comb».ord »-» A;
 }
 
