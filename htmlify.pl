18381a3de93e141228962639b54fe9c34db23fda
Paul Cochrane <paul@liekut.de>
2015-03-18 15:19:21 +0100

    Move categories tables into htmlify.pl
    This is a better place for setup information such as this.

--- htmlify.pl
+++ htmlify.pl
@@ -4,7 +4,7 @@ use lib 'lib';
 use Pod::Htmlify;
 use Perl6::Examples;
 
-my %categories =
+my %base-categories-table =
     "best-of-rosettacode" => "Best of Rosettacode",
     "99-problems"         => "99 problems",
     "cookbook"            => "Cookbook examples",
@@ -20,6 +20,39 @@ my %categories =
     "wsg"                 => "The Winter Scripting Games",
     "other"               => "Uncategorized examples",
 ;
+
+my $all-categories = Categories.new(categories-table => %base-categories-table);
+
+my %cookbook-categories-table =
+    "01strings"                  => "Strings",
+    "02numbers"                  => "Numbers",
+    "03dates-and-times"          => "Dates and Times",
+    "04arrays"                   => "Arrays",
+    "05hashes"                   => "Hashes",
+    "07file-access"              => "File access",
+    "09directories"              => "Directories",
+    "10subroutines"              => "Subroutines",
+    "13classes-objects-and-ties" => "Classes, Objects and Ties",
+;
+
+my $cookbook-categories = Categories.new(categories-table => %cookbook-categories-table);
+$all-categories.append-subcategories(to-category => "cookbook", subcategories => $cookbook-categories);
+
+my %wsg-categories-table =
+    "beginner-2007" => "Beginner-level problems 2007",
+    "beginner-2008" => "Beginner-level problems 2008",
+    "advanced-2008" => "Advanced-level problems 2008",
+;
+
+my $wsg-categories = Categories.new(categories-table => %wsg-categories-table);
+$all-categories.append-subcategories(to-category => "wsg", subcategories => $wsg-categories);
+
+say $all-categories;
+
+exit 0;
+
+my %categories = %base-categories-table;
+my @categories = get-categories(%categories);
 
 my %examples = collect-example-metadata(%categories);
 
--- lib/Perl6/Examples.pm
+++ lib/Perl6/Examples.pm
@@ -28,42 +28,7 @@ class Categories is export {
             take Category.new(key => $subcategory, title => %!categories-table{$subcategory});
         }
     }
-}
 
-my %base-categories-table =
-    "best-of-rosettacode" => "Best of Rosettacode",
-    "99-problems"         => "99 problems",
-    "cookbook"            => "Cookbook examples",
-    "euler"               => "Answers for Project Euler",
-    "games"               => "Games written in Perl 6",
-    "interpreters"        => "Language or DSL interpreters",
-    "module-management"   => "Module management",
-    "parsers"             => "Example grammars",
-    "perlmonks"           => "Answers to perlmonks.org questions",
-    "rosalind"            => "Bioinformatics programming problems",
-    "shootout"            => "The Computer Language Benchmark Game",
-    "tutorial"            => "Tutorial examples",
-    "wsg"                 => "The Winter Scripting Games",
-    "other"               => "Uncategorized examples",
-;
-
-my %cookbook-categories-table =
-    "01strings"                  => "Strings",
-    "02numbers"                  => "Numbers",
-    "03dates-and-times"          => "Dates and Times",
-    "04arrays"                   => "Arrays",
-    "05hashes"                   => "Hashes",
-    "07file-access"              => "File access",
-    "09directories"              => "Directories",
-    "10subroutines"              => "Subroutines",
-    "13classes-objects-and-ties" => "Classes, Objects and Ties",
-;
-
-my %wsg-categories-table =
-    "beginner-2007" => "Beginner-level problems 2007",
-    "beginner-2008" => "Beginner-level problems 2008",
-    "advanced-2008" => "Advanced-level problems 2008",
-;
 
 sub get-categories(%categories) is export {
     my @categories = categories-list(%categories);
@@ -94,7 +59,5 @@ sub categories-list(%categories) {
         take Category.new(key => $subcategory, title => %categories{$subcategory});
     }
 }
-
-my @categories = get-categories(%categories);
 
 # vim: expandtab shiftwidth=4 ft=perl6
