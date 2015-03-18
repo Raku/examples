a8b6ea3c666f391354c37cde5fc013d3bcf7ccba
Paul Cochrane <paul@liekut.de>
2015-03-18 12:18:44 +0100

    Add routines and data to set up (sub)categories

--- lib/Perl6/Examples.pm
+++ lib/Perl6/Examples.pm
@@ -20,4 +20,71 @@ class Category is export {
     has @.subcategories is rw;
 }
 
+my %categories =
+    "best-of-rosettacode" => "Best of Rosettacode",
+    "99-problems"         => "99 problems",
+    "cookbook"            => "Cookbook examples",
+    "euler"               => "Answers for Project Euler",
+    "games"               => "Games written in Perl 6",
+    "interpreters"        => "Language or DSL interpreters",
+    "module-management"   => "Module management",
+    "parsers"             => "Example grammars",
+    "perlmonks"           => "Answers to perlmonks.org questions",
+    "rosalind"            => "Bioinformatics programming problems",
+    "shootout"            => "The Computer Language Benchmark Game",
+    "tutorial"            => "Tutorial examples",
+    "wsg"                 => "The Winter Scripting Games",
+    "other"               => "Uncategorized examples",
+;
+
+my %cookbook-subcategories =
+    "01strings"                  => "Strings",
+    "02numbers"                  => "Numbers",
+    "03dates-and-times"          => "Dates and Times",
+    "04arrays"                   => "Arrays",
+    "05hashes"                   => "Hashes",
+    "07file-access"              => "File access",
+    "09directories"              => "Directories",
+    "10subroutines"              => "Subroutines",
+    "13classes-objects-and-ties" => "Classes, Objects and Ties",
+;
+
+my %wsg-subcategories =
+    "beginner-2007" => "Beginner-level problems 2007",
+    "beginner-2008" => "Beginner-level problems 2008",
+    "advanced-2008" => "Advanced-level problems 2008",
+;
+
+sub get-categories(%categories) {
+    my @categories = init-categories(%categories);
+    @categories = append-subcategories(@categories);
+
+    return @categories;
+}
+
+sub append-subcategories(@categories) {
+    for @categories -> $category {
+        given $category.key {
+            when "cookbook" {
+                $category.subcategories =
+                    init-categories(%cookbook-subcategories);
+            }
+            when "wsg" {
+                $category.subcategories =
+                    init-categories(%wsg-subcategories);
+            }
+        }
+    }
+
+    return @categories;
+}
+
+sub init-categories(%categories) {
+    return gather for %categories.keys -> $subcategory {
+        take Category.new(key => $subcategory, title => %categories{$subcategory});
+    }
+}
+
+my @categories = get-categories(%categories);
+
 # vim: expandtab shiftwidth=4 ft=perl6
