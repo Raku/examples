50fa91f4de82e730b02ef3f5479950f4d50c1709
Paul Cochrane <paul@liekut.de>
2015-04-09 15:50:52 +0200

    Simplify and correct output of P13-topo.pl
    Previous to this commit the code was printing spurious Boolean output in
    between the expected array elements.  This wasn't what was expected
    according to the specifications

--- categories/99-problems/P13-topo.pl
+++ categories/99-problems/P13-topo.pl
@@ -1,37 +1,36 @@
-#!/usr/bin/env perl6
 use v6;
+
+=begin pod
+
+=TITLE P13 - Direct run-length encoding.
+
+=AUTHOR Ryan Connelly
+
+=head1 Example
+
+    > say encode(<a a a a b c c a a d e e e e>.list).perl;
+    ([4, "a"], "b", [2, "c"], [2, "a"], "d", [4, "e"]).list
+
+=end pod
 
 # I can't see the difference between this and P11.
 # Perhaps I'm being stupid.
 
 sub encode(@list)
 {
-    gather while @list.elems
-    {
+    gather while @list.elems {
         my $value = @list[0];
         my $count = 0;
 
-        take (
-            while @list.elems
-              and @list[0] ~~ $value
-            {
-                $count++;
-                shift @list
-            }
+        while @list.elems and @list[0] ~~ $value {
+            $count++;
+            shift @list
+        }
 
-            $count == 1 ?? $value !! [$count, $value]
-        )
+        take $count == 1 ?? $value !! [$count, $value];
     }
 }
 
 say encode(<a a a a b c c a a d e e e e>.list).perl;
-
-=begin pod
-
-=head1 NAME
-
-P13 - Direct run-length encoding.
-
-=end pod
 
 # vim: expandtab shiftwidth=4 ft=perl6
