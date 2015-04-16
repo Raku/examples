e0eda16fd9b82e005c64e41fd5ce6792c5700914
Steve Mynott <steve.mynott@gmail.com>
2015-04-16 22:04:46 +0100

    more 6ish

--- categories/cookbook/17sockets/17-01tcp_client.pl
+++ categories/cookbook/17sockets/17-01tcp_client.pl
@@ -10,7 +10,7 @@ You want to connect to a socket on a remote host
 my $s = IO::Socket::INET.new( :host<www.perl6.org>, :port(80) );
 $s.send( "HEAD / HTTP/1.0\n\n" );
 
-while ( my $r = $s.get ) {
+while my $r = $s.get {
     say $r;
 }
 
