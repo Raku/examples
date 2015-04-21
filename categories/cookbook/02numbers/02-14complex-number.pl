7db1122edbb73c4d5ac31f541757a13570e30f0d
Steve Mynott <steve.mynott@gmail.com>
2015-04-21 22:10:02 +0100

    simple complex nos, base convert and tweaking

---
+++ categories/cookbook/02numbers/02-14complex-number.pl
@@ -0,0 +1,17 @@
+use v6;
+
+=begin pod
+
+=TITLE Complex Numbers
+
+=AUTHOR stmuk
+
+Math with Complex Numbers
+
+=end pod
+
+say (3+5i) * (2-2i); # 16+4i
+
+say sqrt(3+4i); # 2+1i
+
+# vim: expandtab shiftwidth=4 ft=perl6
