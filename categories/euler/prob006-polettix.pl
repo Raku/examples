e152d659f2345b7fdc1a04c843f8ff7fd405fe1f
Paul Cochrane <paul@liekut.de>
2015-04-22 18:52:08 +0200

    Remove spurious backquote

--- categories/euler/prob006-polettix.pl
+++ categories/euler/prob006-polettix.pl
@@ -22,7 +22,7 @@ ten natural numbers and the square of the sum is 3025 - 385 = 2640.
 Find the difference between the sum of the squares of the first
 one hundred natural numbers and the square of the sum.
 
-Expected result: 25164150`
+Expected result: 25164150
 
 =end pod
 
