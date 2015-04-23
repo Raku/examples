3ce582d1b4ceb38869de13dd7552798d09f367c3
Paul Cochrane <paul@liekut.de>
2015-04-23 14:27:36 +0200

    Make verbose progress output optional

--- categories/euler/prob168-shlomif.pl
+++ categories/euler/prob168-shlomif.pl
@@ -20,25 +20,32 @@ Expected result: 59206
 
 =end pod
 
-my $sum = 0;
-# $multiplier is "d"
-for 1 .. 9 -> $multiplier {
-    for 1 .. 99 -> $L {
-        # $digit is m.
-        for 1 .. 9 -> $digit {
-            my $n = (((10 ** $L - $multiplier)*$digit)/(10*$multiplier - 1));
+sub MAIN(Bool :$verbose = False) {
+    my $sum = 0;
+    # $multiplier is "d"
+    for 1 .. 9 -> $multiplier {
+        for 1 .. 99 -> $L {
+            # $digit is m.
+            for 1 .. 9 -> $digit {
+                my $n = (((10 ** $L - $multiplier)*$digit)/(10*$multiplier - 1));
 
-            my $number_to_check = $n * 10 + $digit;
-            if ($n.chars() == $L and ($multiplier * $number_to_check
-                    == $n + $digit * 10 ** $L)) {
-                print "Found $number_to_check\n";
-                $sum += $number_to_check;
-                print "Sum = $sum\n";
+                my $number_to_check = $n * 10 + $digit;
+                if ($n.chars() == $L and ($multiplier * $number_to_check
+                        == $n + $digit * 10 ** $L)) {
+                    print "Found $number_to_check\n" if $verbose;
+                    $sum += $number_to_check;
+                    print "Sum = $sum\n" if $verbose;
+                }
             }
         }
     }
-}
 
-say "Last 5 digits of the final sum are: ", "$sum".substr(*-5);
+    if $verbose {
+        say "Last 5 digits of the final sum are: ", "$sum".substr(*-5);
+    }
+    else {
+        say "$sum".substr(*-5);
+    }
+}
 
 # vim: expandtab shiftwidth=4 ft=perl6
