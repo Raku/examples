37f017e1dc4f7970b54adcb1646ebaf6cbfce839
Paul Cochrane <paul@liekut.de>
2015-04-28 21:06:19 +0200

    Wrap text to within 80 chars
    This is consistent with the other examples and makes the text easy to read
    in all editors.

--- categories/euler/prob100-andreoss.pl
+++ categories/euler/prob100-andreoss.pl
@@ -8,11 +8,17 @@ use v6;
 
 L<https://projecteuler.net/problem=100>
 
-If a box contains twenty-one coloured discs, composed of fifteen blue discs and six red discs, and two discs were taken at random, it can be seen that the probability of taking two blue discs, P(BB) = (15/21)×(14/20) = 1/2.
+If a box contains twenty-one coloured discs, composed of fifteen blue discs
+and six red discs, and two discs were taken at random, it can be seen that
+the probability of taking two blue discs, P(BB) = (15/21)×(14/20) = 1/2.
 
-The next such arrangement, for which there is exactly 50% chance of taking two blue discs at random, is a box containing eighty-five blue discs and thirty-five red discs.
+The next such arrangement, for which there is exactly 50% chance of taking
+two blue discs at random, is a box containing eighty-five blue discs and
+thirty-five red discs.
 
-By finding the first arrangement to contain over 10¹² = 1,000,000,000,000 discs in total, determine the number of blue discs that the box would contain.
+By finding the first arrangement to contain over 10¹² = 1,000,000,000,000
+discs in total, determine the number of blue discs that the box would
+contain.
 
 Expected result: 756872327473
 
