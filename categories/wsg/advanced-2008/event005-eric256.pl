f3d5a86ae97790024046d6f8eb28efd67afe722d
Paul Cochrane <paul@liekut.de>
2015-04-28 08:11:33 +0200

    No longer looping indefinitely
    Simply running the program for the first password passed in.

--- categories/wsg/advanced-2008/event005-eric256.pl
+++ categories/wsg/advanced-2008/event005-eric256.pl
@@ -148,48 +148,46 @@ L<http://web.archive.org/web/20080410170315/http://www.microsoft.com/technet/scr
 my $input-file = $*PROGRAM_NAME.IO.dirname ~ "/wordlist.txt";
 my %dict = ( ($input-file.IO.slurp.split("\n").grep: {.chars > 6}) X 1);
 
-loop {
-    my $password = prompt("Enter password to test: ");
+my $password = prompt("Enter password to test: ");
 
-    say "Testing strength of password '$password'";
+say "Testing strength of password '$password'";
 
-    my $score = 13;
+my $score = 13;
 
-    if %dict{$password} :exists {
-        $score--;
-        say "Password matched dictionary";
-    }
-
-    if %dict{$password.substr(0, $password.chars -1 )} :exists {
-        $score--;
-        say "Password minus last char is in dictionary";
-    }
-
-    if %dict{$password.substr(1,$password.chars-1)} :exists {
-        $score--;
-        say "Password minus first char is in dictionary";
-    }
-
-    my $test = $password;
-    $test.subst(/0/, "o");
-    if %dict{$test} :exists {
-        $score--;
-        say "Password replaces 'o' with '0'";
-    }
-
-    $test = $password;
-    $test.subst(/1/, "i");
-    if %dict{$test} :exists {
-        $score--;
-        say "Password replaces 'i' with '1'";
-    }
-
-    if $password.chars == none(10..20) {
-        $score--;
-        say "Password is too short (less than 10) or too long (more than 20)"
-    }
-
-    say "'$password' scored $score";
+if %dict{$password} :exists {
+    $score--;
+    say "Password matched dictionary";
 }
+
+if %dict{$password.substr(0, $password.chars -1 )} :exists {
+    $score--;
+    say "Password minus last char is in dictionary";
+}
+
+if %dict{$password.substr(1,$password.chars-1)} :exists {
+    $score--;
+    say "Password minus first char is in dictionary";
+}
+
+my $test = $password;
+$test.subst(/0/, "o");
+if %dict{$test} :exists {
+    $score--;
+    say "Password replaces 'o' with '0'";
+}
+
+$test = $password;
+$test.subst(/1/, "i");
+if %dict{$test} :exists {
+    $score--;
+    say "Password replaces 'i' with '1'";
+}
+
+if $password.chars == none(10..20) {
+    $score--;
+    say "Password is too short (less than 10) or too long (more than 20)"
+}
+
+say "'$password' scored $score";
 
 # vim: expandtab shiftwidth=4 ft=perl6
