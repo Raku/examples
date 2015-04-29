81ec7f0d9a344eb946f771c26d08650e4e14fd1e
Paul Cochrane <paul@liekut.de>
2015-04-29 17:47:00 +0200

    Reduce verbosity of phone number example output

--- categories/wsg/advanced-2008/event001-eric256.pl
+++ categories/wsg/advanced-2008/event001-eric256.pl
@@ -112,37 +112,40 @@ L<http://web.archive.org/web/20080321224441/http://www.microsoft.com/technet/scr
 
 =end pod
 
-my $input-file = $*PROGRAM_NAME.IO.dirname ~ "/wordlist.txt";
-my %dict = ( ($input-file.IO.slurp.split("\n").grep: {.chars == 7}) X 1);
+sub MAIN(Bool :$verbose = False) {
+    my $input-file = $*PROGRAM_NAME.IO.dirname ~ "/wordlist.txt";
+    my %dict = ( ($input-file.IO.slurp.split("\n").grep: {.chars == 7}) X 1);
 
-my %digits = (
-    2 => (<a b c>),
-    3 => (<d e f>),
-    4 => (<g h i>),
-    5 => (<j k l>),
-    6 => (<m n o>),
-    7 => (<p r s>),
-    8 => (<t u v>),
-    9 => (<w x y>),
-);
+    my %digits = (
+        2 => (<a b c>),
+        3 => (<d e f>),
+        4 => (<g h i>),
+        5 => (<j k l>),
+        6 => (<m n o>),
+        7 => (<p r s>),
+        8 => (<t u v>),
+        9 => (<w x y>),
+    );
 
-my $phone_number = 7323464;
-my @test_words;
+    my $phone_number = 7323464;
+    my @test_words;
 
-for $phone_number.split('') {
-    say $_, "->", %digits{$_}.join('-');
-    if (@test_words.elems) {
-        my @values = %digits{$_}.values;
-        @test_words = @test_words X~ '' X~ @values;
+    for $phone_number.split('') {
+        say $_, "->", %digits{$_}.join('-') if $verbose;
+        if (@test_words.elems) {
+            my @values = %digits{$_}.values;
+            @test_words = @test_words X~ '' X~ @values;
+        }
+        else {
+            @test_words = %digits{$_}.values;
+        }
     }
-    else {
-        @test_words = %digits{$_}.values;
-    }
-}
 
-say "Comparing {@test_words.elems} words against a dictionary of {%dict.elems} entries";
-for @test_words -> $word {
-    $word.say if defined %dict{$word};
+    say "Comparing {@test_words.elems} words against a dictionary of {%dict.elems} entries"
+        if $verbose;
+    for @test_words -> $word {
+        $word.uc.say if defined %dict{$word};
+    }
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
