0c6ea296ecd8d8eb51d16c5dc28375a59c26e424
Paul Cochrane <paul@liekut.de>
2015-05-06 14:25:36 +0200

    Trim more trailing whitespace

--- categories/cookbook/01strings/01-13upper-lower-case.pl
+++ categories/cookbook/01strings/01-13upper-lower-case.pl
@@ -12,10 +12,10 @@ You have a string and what to upper/lower case it
 
 my $string = "the cat sat on the mat";
 
-say $string=$string.uc; # THE CAT SAT ON THE MAT 
+say $string=$string.uc; # THE CAT SAT ON THE MAT
 
-say $string.=lc; # the cat sat on the mat 
+say $string.=lc; # the cat sat on the mat
 
-say $string.wordcase; # The Cat Sat On The Mat 
+say $string.wordcase; # The Cat Sat On The Mat
 
 # vim: expandtab shiftwidth=4 ft=perl6
