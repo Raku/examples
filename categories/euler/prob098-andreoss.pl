f7704d1b17cd522b82c64c142d4483dc9c1bad80
Paul Cochrane <paul@liekut.de>
2015-05-09 09:38:47 +0200

    Wrap description text for readability

--- categories/euler/prob098-andreoss.pl
+++ categories/euler/prob098-andreoss.pl
@@ -8,14 +8,22 @@ use v6;
 
 L<https://projecteuler.net/problem=98>
 
-By replacing each of the letters in the word CARE with 1, 2, 9, and 6 respectively, we form a square number: 1296 = 36². What is remarkable is that, by using the same digital substitutions, the anagram, RACE, also forms a square number: 9216 = 96². We shall call CARE (and RACE) a square anagram word pair and specify further that leading zeroes are not permitted, neither may a different letter have the same digital value as another letter.
+By replacing each of the letters in the word CARE with 1, 2, 9, and 6
+respectively, we form a square number: 1296 = 36². What is remarkable is
+that, by using the same digital substitutions, the anagram, RACE, also forms
+a square number: 9216 = 96². We shall call CARE (and RACE) a square anagram
+word pair and specify further that leading zeroes are not permitted, neither
+may a different letter have the same digital value as another letter.
 
-Using words.txt L<https://projecteuler.net/project/resources/p098_words.txt>, a 16K text file containing nearly two-thousand common English words, find all the square anagram word pairs (a palindromic word is NOT considered to be an anagram of itself).
+Using words.txt
+L<https://projecteuler.net/project/resources/p098_words.txt>, a 16K text
+file containing nearly two-thousand common English words, find all the
+square anagram word pairs (a palindromic word is NOT considered to be an
+anagram of itself).
 
 What is the largest square number formed by any member of such a pair?
 
 NOTE: All anagrams formed must be contained in the given text file.
-
 
 Expected result:  18769
 
