6e7c4750a8fb205594f3b855690afd878af56b60
Paul Cochrane <paul@liekut.de>
2015-05-15 10:37:29 +0200

    [cookbook] split lines for improved readability

--- categories/cookbook/14database-access/14-09-dbi-sql.pl
+++ categories/cookbook/14database-access/14-09-dbi-sql.pl
@@ -29,12 +29,12 @@ $dbh.do($sql);
 my $st = $dbh.prepare('INSERT INTO video(title, uri) VALUES(?, ?)');
 
 # put some data in
-
-$st.execute("Larry Wall - Keynote, APW2014 2014-10-10 ", "https://www.youtube.com/watch?v=enlqVqit62Y");
-$st.execute("Carl Mäsak - Regexes in Perl 6 - Zero to Perl 6 Training", "https://www.youtube.com/watch?v=oo-gA9Z9SaA");
+$st.execute("Larry Wall - Keynote, APW2014 2014-10-10 ",
+            "https://www.youtube.com/watch?v=enlqVqit62Y");
+$st.execute("Carl Mäsak - Regexes in Perl 6 - Zero to Perl 6 Training",
+            "https://www.youtube.com/watch?v=oo-gA9Z9SaA");
 
 # get some data out
-
 $st = $dbh.prepare('SELECT title,uri FROM video');
 $st.execute;
 
