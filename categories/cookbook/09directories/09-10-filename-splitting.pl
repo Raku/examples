7c839b9ed89fb5d513edca976d2bd544e704b5b8
Paul Cochrane <paul@liekut.de>
2015-05-19 08:02:31 +0200

    [cookbook] remove extra semicolon

--- categories/cookbook/09directories/09-10-filename-splitting.pl
+++ categories/cookbook/09directories/09-10-filename-splitting.pl
@@ -13,7 +13,7 @@ You want to split a filename into basename, directory etc.
 my $io = IO::Path.new-from-absolute-path("/usr/lib64/libc.so");
 
 say $io.basename;
-say $io.dirname;;
+say $io.dirname;
 say $io.extension;
 
 # vim: expandtab shiftwidth=4 ft=perl6
