889bd653034f6818d948d815e02917423185a2b9
andreoss <andreoss@sdf.org>
2015-05-19 17:34:53 -0400

    [euler] pod formating

--- categories/euler/prob066-andreoss.pl
+++ categories/euler/prob066-andreoss.pl
@@ -8,9 +8,7 @@ use v6;
 
 L<https://projecteuler.net/problem=66>
 
-Consider quadratic Diophantine equations of the form:
-
-x² – D×y² = 1
+Consider quadratic Diophantine equations of the form: x² – D×y² = 1
 
 For example, when D=13, the minimal solution in x is 649² – 13×180² = 1.
 
@@ -20,12 +18,16 @@ square.
 By finding minimal solutions in x for D = {2, 3, 5, 6, 7}, we obtain the
 following:
 
-3²– 2×2²= 1
-2²– 3×1²= 1
-9²– 5×4²= 1
-5²– 6×2²= 1
-8²– 7×3²= 1
+3² – 2×2²= 1
 
+2² – 3×1²= 1
+
+9² – 5×4²= 1
+
+5² – 6×2²= 1
+
+8² – 7×3²= 1
+	
 Hence, by considering minimal solutions in x for D ≤ 7, the largest x is
 obtained when D=5.
 
