34504cceae098a0ee88863f4b71641e9719e79db
Paul Cochrane <paul@liekut.de>
2015-05-19 23:03:28 +0200

    [interpreters] replace tabs with spaces
     - add vim coda
     - remove trailing whitespace

--- categories/interpreters/lisp.pl
+++ categories/interpreters/lisp.pl
@@ -1,125 +1,122 @@
-#!/usr/bin/env perl6
 use v6;
 
 =begin pod
 
-=AUTHOR Andrei Osipov
+=TITLE Simple Lisp Interpreter
 
-=TITLE Simple Lisp Interpreter 
+=AUTHOR Andrei Osipov
 
 Inspired by L<http://www.norvig.com/lispy.html>
 
 =end pod
 
-
 subset Number of Str where  -> $x {
-	so $x ~~ /^^ <[-+]>?\d+ ( '.' \d* )? $$/ 
+    so $x ~~ /^^ <[-+]>?\d+ ( '.' \d* )? $$/
 }
 
 class Func {
-	has Callable $.code;
-	has Str      $.desc;
-	method eval(@a) { $.code.(|@a)   }
-	method gist     { "#<{$.desc}>" }
+    has Callable $.code;
+    has Str      $.desc;
+    method eval(@a) { $.code.(|@a)   }
+    method gist     { "#<{$.desc}>" }
 }
 
 class Symbol {
-	has $.name;
-	method CALL-ME($x) {
-		Symbol.new(name => $x);
-	}
-	method Str  { $.name }
+    has $.name;
+    method CALL-ME($x) {
+        Symbol.new(name => $x);
+    }
+    method Str  { $.name }
 }
 
-
 class Env {
-	has       %.scope;
-	has  Env  $.outer;
-	method resolve($key) is rw {
-		if %.scope{$key}:exists {
-			%.scope{$key}
-		}
-		else {
-			fail "Not found symbol '$key'" unless $.outer;
-			$.outer.resolve($key)
-		}
-	}
-	method merge(*@env) {
-		%.scope = %.scope, %(@env)
-	}
-	multi method evaluate-tokens(Number $x) {
-		$x
-	}
-	multi method evaluate-tokens(Symbol $x) {
-		self.resolve($x)
-	}
-	multi method evaluate-tokens(Positional $x) {
-		my @x = @($x);
-		fail "Syntax error" if +@x == 0;
-		my $verb = @x.shift;
-		given $verb {
-			when 'quote'   {
-				if @x[0] ~~ Positional {
-					[ @x[0] ];
-				}
-				else { @x[0] }
-			}
-			when 'if'      {
-				my ($test,
-				$conseq,
-				$alt) = @x;
-				self.evaluate-tokens(
-					self.evaluate-tokens($test)
-					?? $conseq
-					!! $alt
-				)
-			}
-			when 'set!'    {
-				my ($var, $exp) = @x;
-				self.resolve($var) = self.evaluate-tokens($exp);
-				#return $var;
+    has       %.scope;
+    has  Env  $.outer;
+    method resolve($key) is rw {
+        if %.scope{$key}:exists {
+            %.scope{$key}
+        }
+        else {
+            fail "Not found symbol '$key'" unless $.outer;
+            $.outer.resolve($key)
+        }
+    }
+    method merge(*@env) {
+        %.scope = %.scope, %(@env)
+    }
+    multi method evaluate-tokens(Number $x) {
+        $x
+    }
+    multi method evaluate-tokens(Symbol $x) {
+        self.resolve($x)
+    }
+    multi method evaluate-tokens(Positional $x) {
+        my @x = @($x);
+        fail "Syntax error" if +@x == 0;
+        my $verb = @x.shift;
+        given $verb {
+            when 'quote'   {
+                if @x[0] ~~ Positional {
+                    [ @x[0] ];
+                }
+                else { @x[0] }
+            }
+            when 'if'      {
+                my ($test,
+                $conseq,
+                $alt) = @x;
+                self.evaluate-tokens(
+                    self.evaluate-tokens($test)
+                    ?? $conseq
+                    !! $alt
+                )
+            }
+            when 'set!'    {
+                my ($var, $exp) = @x;
+                self.resolve($var) = self.evaluate-tokens($exp);
+                #return $var;
 
-			}
-			when 'define'  {
-				my ($var, $exp) = @x; 
-				if $var ~~ Positional {
-					$.scope{$var[0]} = 
-					self.evaluate-tokens([ Symbol('λ'), [ $var[1..*] ], $exp]); 
-				} 
-				else { $.scope{$var}  =self.evaluate-tokens($exp); }
-			}
-			when 'lambda' | 'λ' {
-				my ($vars, $exp) = @x;
-				Func.new( code => -> *@argv {
-					my %x = ($vars.list Z @argv);
-					my $new-env = Env.new(scope => %x , outer => self);
-					$new-env.evaluate-tokens($exp)
-				},
-				desc => "closure:arity:{$vars.elems}" );
+            }
+            when 'define'  {
+                my ($var, $exp) = @x;
+                if $var ~~ Positional {
+                    $.scope{$var[0]} =
+                    self.evaluate-tokens([ Symbol('λ'), [ $var[1..*] ], $exp]);
+                }
+                else { $.scope{$var}  =self.evaluate-tokens($exp); }
+            }
+            when 'lambda' | 'λ' {
+                my ($vars, $exp) = @x;
+                Func.new( code => -> *@argv {
+                    my %x = ($vars.list Z @argv);
+                    my $new-env = Env.new(scope => %x , outer => self);
+                    $new-env.evaluate-tokens($exp)
+                },
+                desc => "closure:arity:{$vars.elems}" );
 
-			}
-			when 'begin'   {
-				my $val;
-				for @x -> $exp {
-					$val = self.evaluate-tokens($exp);
-				}
-				$val;
-			}
-			default {
-				my $func = self.evaluate-tokens($verb);
-				my @args = map {
-					self.evaluate-tokens($^x)
-				}, @x;
-				fail "$verb is not a function" unless
-				$func ~~ Func;
-				$func.eval(@args)
+            }
+            when 'begin'   {
+                my $val;
+                for @x -> $exp {
+                    $val = self.evaluate-tokens($exp);
+                }
+                $val;
+            }
+            default {
+                my $func = self.evaluate-tokens($verb);
+                my @args = map {
+                    self.evaluate-tokens($^x)
+                }, @x;
+                fail "$verb is not a function" unless
+                $func ~~ Func;
+                $func.eval(@args)
 
 
-			}
+            }
 
-		}
+        }
 
-	}
+    }
 }
 
 our %*BUILTINS    =
@@ -129,7 +126,7 @@ our %*BUILTINS    =
 '/'          => -> *@a { [/] @a },
 'abs'        => -> $a { abs $a },
 'not'        => -> $a { not $a } ,
-'so'         => -> $a { so  $a } , 
+'so'         => -> $a { so  $a } ,
 '>'          => -> *@a { [>] @a },
 '<'          => -> *@a { [<] @a },
 '>='         => -> *@a { [>=]  @a },
@@ -137,28 +134,28 @@ our %*BUILTINS    =
 '='          => -> *@a { [==]  @a },
 'equal?'     => -> *@a { [~~]  @a },
 'length' => -> $a {
-	$a[0].elems 
+        $a[0].elems
 },
 'cons'   => -> *@a { [ @a.item ]},
 'car'    => ->  @a {  @a[0] },
 'cdr'    => ->  @a {
-	@a[1...*]
+        @a[1...*]
 },
 'append' => -> *@a {
-	my @x =  @a[0][0..*];
-	@x.push: @a[1];
-	@x;
+        my @x =  @a[0][0..*];
+        @x.push: @a[1];
+        @x;
 },
 'list'   => -> *@a { [ @a.item ]  },
 'list?'  => ->  $a  { so $a ~~ Positional },
 'null?'  => -> Positional $a  {
-	$a.elems == 0
+        $a.elems == 0
 },
 'symbol?' => -> *@a {
-	#so @a ~~ Str
+        #so @a ~~ Str
 },
 'display' => -> *@a {
-	say join ', ', @a.map(*.Str); 
+        say join ', ', @a.map(*.Str);
 },
 'exit'    => -> $a { exit $a }
 ;
@@ -171,11 +168,11 @@ map { $_.key => Func.new(code => $_.value, desc => "builtin:{$_.key}") }, %*BUIL
 our $*LISP-ENV = Env.new(scope => %*LISP-GLOBAL);
 
 sub tokenize(Str $s) {
-	$s.trans(
-		<()> => [' ( ', ' ) ' ])\
-			.split(/\s+/, :g)\
-			.grep(*.chars);
-}
+    $s.trans(
+        <()> => [' ( ', ' ) ' ])\
+            .split(/\s+/, :g)\
+                .grep(*.chars);
+            }
 
 multi read-from-tokens([]) {
     fail  "unexpected EOF while reading"
@@ -184,15 +181,15 @@ multi read-from-tokens([]) {
 multi read-from-tokens(@tokens is rw) {
     my $token = @tokens.shift;
     given $token {
-	when '(' {
-	    fail "unexpected EOF while reading" unless @tokens;
-	    my @x = gather while @tokens[0] ne ')' {
-		take read-from-tokens(@tokens)
-	    }
-	    @tokens.shift;
-	    return @x.item;
-	}
-	when ')' { fail "unexpected ')'" }
+        when '(' {
+            fail "unexpected EOF while reading" unless @tokens;
+            my @x = gather while @tokens[0] ne ')' {
+                take read-from-tokens(@tokens)
+            }
+            @tokens.shift;
+            return @x.item;
+        }
+        when ')' { fail "unexpected ')'" }
     }
 
     atom $token;
@@ -217,39 +214,38 @@ sub eval(Str $sexp) {
 sub balanced($s) {
     my $l = 0;
     for $s.comb {
-	when ")" {
-	    --$l;
-	    #return False if $l < 0;
-	}
-	when "(" {
-	    ++$l;
-	}
+        when ")" {
+            --$l;
+            #return False if $l < 0;
+        }
+        when "(" {
+            ++$l;
+        }
     }
     $l ;
 }
 
-
 sub repl {
     my Str $exp = '';
-    my Int $balance = 0; 
+    my Int $balance = 0;
     loop {
-	try {
-	    exit unless defined my $p =  prompt($exp eq '' ?? "> " !! ("--" xx $balance) ~ "> ");
-	    $exp ~= "$p ";
-	    $exp ~~ s:i/ ';' ** 1..* .*? $$//; 
-	    $balance = balanced $exp;
-	    fail "unexpected bracket" if $balance < 0;
-	    next if $balance != 0 || $exp !~~ /\S+/;
+        try {
+            exit unless defined my $p =  prompt($exp eq '' ?? "> " !! ("--" xx $balance) ~ "> ");
+            $exp ~= "$p ";
+            $exp ~~ s:i/ ';' ** 1..* .*? $$//;
+            $balance = balanced $exp;
+            fail "unexpected bracket" if $balance < 0;
+            next if $balance != 0 || $exp !~~ /\S+/;
 
-	    my $result = eval $exp;
-	    say ";; " ~ $result.&to-string;
-	    CATCH {
-		default {
-		    say "error: $_";
-		}
-	    }
-	}
-	$exp = '';
+            my $result = eval $exp;
+            say ";; " ~ $result.&to-string;
+            CATCH {
+                default {
+                    say "error: $_";
+                }
+            }
+        }
+        $exp = '';
     }
 }
 
@@ -267,25 +263,24 @@ multi to-string(Symbol $exp) { $exp }
 multi to-string(Bool $x ) { $x ?? "#t" !! "#f" }
 multi to-string(Any $x) { $x.perl }
 
-
 sub MAIN(Bool :$run-tests = False,
-	 Str  :$file) {
+         Str  :$file) {
     if $file {
-	die "Can't open '$file'" unless $file.IO.f;
-	my $exp;
-	for $file.IO.lines {
-	    my $line = $_;
-	    $line ~~ s:i/ ';' * 1..*  .*? $$ //;
-	    $exp ~= $line;
-	    if balanced($exp) == 0 {
-		eval $exp;
-		$exp = '';
-	    }
-	}
-	return;
+        die "Can't open '$file'" unless $file.IO.f;
+        my $exp;
+        for $file.IO.lines {
+            my $line = $_;
+            $line ~~ s:i/ ';' * 1..*  .*? $$ //;
+            $exp ~= $line;
+            if balanced($exp) == 0 {
+                eval $exp;
+                $exp = '';
+            }
+        }
+        return;
     }
     if $run-tests {
-	return tests;
+        return tests;
     }
     repl;
 }
@@ -314,14 +309,14 @@ sub tests {
     ok eval("(equal? 1 1)") ,"equal?";
     ok !eval("(equal? 1 0)") ,"equal?";
     #ok !eval("(symbol? 1)") ,"symbol?"; # todo
-					 #ok eval("(symbol? +)") ,"symbol?";  # todo
-									      ok eval("(define xxx 1)") == 1 ,"define";
+                                         #ok eval("(symbol? +)") ,"symbol?";  # todo
+                                                                              ok eval("(define xxx 1)") == 1 ,"define";
     eval("(set! xxx 2)");
     ok eval("xxx") == 2, 'set!';
     ok eval("(define xs (list 1 2 3 4))") == [[1,2,3,4]] ,"define";
     ok eval("(define sqr (lambda (x) (* x x)))") , 'define'; ;
     ok eval("(length xs)") == 4, 'length';
-    ok eval("((lambda (x) (* x x)) 13))") == 169, 'lambda'; 
+    ok eval("((lambda (x) (* x x)) 13))") == 169, 'lambda';
     ok eval("(sqr 10)") == 100, 'lambda';
     ok eval("(define plus (lambda (x y) (+ x y)))") &&
     eval("(plus 1 2)") == 3, "lambda";
@@ -334,22 +329,24 @@ sub tests {
     ok eval(qq{ (define fib (lambda (n)  (if (< n 2)  1  (+ (fib (- n 1)) (fib (- n 2)))))) })  &&
     eval("(fib 10)") == 89, 'fib(10)';
     eval(qq{
-		(define sqrt
-		 (lambda (x)
-		  (begin
-		   (define square (lambda (x) (* x x)))
-		   (define average (lambda (x y) (/ (+ x y) 2)))
-		   (define good-enough?
-		    (lambda (guess)
-		     (< (abs (- (square guess) x)) 0.001)))
-		   (define improve (lambda (guess)
-				    (average guess (/ x guess))))
-		   (define sqrt-iter (lambda (guess)
-				      (if (good-enough? guess)
-				       guess
-				       (sqrt-iter (improve guess)))))
-		   (sqrt-iter 1.0)))) });
+            (define sqrt
+             (lambda (x)
+              (begin
+               (define square (lambda (x) (* x x)))
+               (define average (lambda (x y) (/ (+ x y) 2)))
+               (define good-enough?
+                (lambda (guess)
+                 (< (abs (- (square guess) x)) 0.001)))
+               (define improve (lambda (guess)
+                                (average guess (/ x guess))))
+               (define sqrt-iter (lambda (guess)
+                                  (if (good-enough? guess)
+                                   guess
+                                   (sqrt-iter (improve guess)))))
+               (sqrt-iter 1.0)))) });
     ok eval("(sqrt 4)").Int == 2, 'sqrt example';
 
     done;
 }
+
+# vim: expandtab shiftwidth=4 ft=perl6
