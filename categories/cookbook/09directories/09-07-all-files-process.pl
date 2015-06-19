7e4b07d1f85aa62a3793ce5c958b66dd3cc2ab2e
Paul Cochrane <paul@liekut.de>
2015-06-19 23:04:09 +0200

    [cookbook] pass dir to process as cmd line arg
    Also sort files as their abspath is printed.  This helps give consistent
    output when testing.

--- categories/cookbook/09directories/09-07-all-files-process.pl
+++ categories/cookbook/09directories/09-07-all-files-process.pl
@@ -12,14 +12,16 @@ You want to recurse over all files in and under a directory
 
 use File::Find;
 
-# note binding := for a list
+sub MAIN(:$dir = "/etc") {
+    # note binding := for a list
 
-my @files := find(:dir("/etc"), :type('file'));
+    my @files := find(:dir($dir), :type('file'));
 
-#  returns a list of IO::Path objects
+    #  returns a list of IO::Path objects
 
-for @files -> $io {
-    say $io.abspath;
+    for @files.sort -> $io {
+        say $io.abspath;
+    }
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
