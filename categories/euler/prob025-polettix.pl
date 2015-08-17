fc16c647d8c3cc7c4c631b1a8a413bbaeb6084fb
Flavio Poletti <flavio@polettix.it>
2015-08-18 01:40:43 +0200

    bigints are now available... yay!

--- categories/euler/prob025-polettix.pl
+++ categories/euler/prob025-polettix.pl
@@ -32,68 +32,17 @@ What is the first term in the Fibonacci sequence to contain 1000 digits?
 
 =end pod
 
-our $limit;
-our $digits;
-# You can optionally pass how many digits you're looking for, defaults
-# to the request in the original challenge.
-sub MAIN(Int :$length = 1000, Bool :$verbose = False) {
-    # As of August 12, 2009 we don't have big integers, so we'll have
-    # to conjure up something. We'll represent each number with an
-    # array of digits, base 10 for ease of length computation. The most
-    # significant part is at the end of the array, i.e. the array should
-    # be read in reverse.
-    $digits = 10;
-    $limit = 10 ** $digits;
-    my @x = (0);
-    my @y = (1);
-
-    # This will count the n-th Fibonacci number
-    my $c = 1;
-    my $current_length = 1;
-    while ($current_length < $length) {
-        ++$c;
-
-        # (x, y) = (y, x + y)
-        my @z = @y;
-        addto(@y, @x); # modifies @y in-place
-        @x = @z;
-
-        # The most significant part of the number is in the last element
-        # of the array; every other element is $digits bytes long by
-        # definition.
-        my $msb = printable([@y[*-1]]);
-        $current_length = $msb.encode('utf-8').bytes + (@y - 1) * $digits;
-
-        # Print a feedback every 20 steps
-        if $verbose {
-            say "$c -> $current_length" unless $c % 100;
-        }
+sub MAIN(Int :$length = 1000, Bool :$boring = False) {
+    if $boring {
+        my ($x, $y, $c) = (1, 1, 2);
+        ($x, $y, $c) = ($y, $x + $y, $c + 1) while $y.chars < $length;
+        $c.say;
+        return;
     }
 
-    say $c;
-}
-
-# Add a "number" to another, modifies first parameter in place.
-# This assumes that length(@y) <= length(@x), which will be true in
-# our program because @y is lower than @x
-sub addto (@x is rw, @y) {
-    my $rest = 0;
-    # Assuming length(@y) <= length(@x) means that we have to
-    # put "0"s to iterate over the whole $x. This could be
-    # improved, but it's unlikely that two consecutive Fibonacci
-    # numbers differ by more than one digit
-    for (@x Z (@y, 0, *)).flat -> $x is rw, $y {
-        $x += $y + $rest;
-        $rest = ($x / $limit).Int;
-        $x %= $limit;
-    }
-    push @x, $rest if $rest;
+    my @fibs := 0, 1, *+* ... *;
+    ((1..*).grep:{@fibs[$_].chars == $length})[0].say;
     return;
-}
-
-sub printable (@x is copy) {
-    my $msb = pop @x;
-    return $msb ~ @x.reverse.map({sprintf '%0'~$digits~'d', $_ }).join('');
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
