29399c321654a2d5de968c498d8db2749e11c564
Steve Mynott <steve.mynott@gmail.com>
2015-09-09 17:39:15 +0100

    fix one more GLR test

--- categories/99-problems/P36-ovid.pl
+++ categories/99-problems/P36-ovid.pl
@@ -118,7 +118,7 @@ sub prime-factors(Int $number is copy) {
 }
 
 for 17, 53, 90, 94, 200, 289, 62710561 -> $number {
-    say "Prime factors of $number are: ", prime-factors($number).perl;
+    say "Prime factors of $number are: ", prime-factors($number).flat.sort.list.perl;
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
