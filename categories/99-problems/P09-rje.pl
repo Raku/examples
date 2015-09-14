3b341ad4740f943fd6920e6433c6709f88b63928
Paul Cochrane <paul@liekut.de>
2015-09-14 13:45:02 +0200

    [99probs] minor reformatting, indenting etc.
    Now things are a bit more consistent.

--- categories/99-problems/P09-rje.pl
+++ categories/99-problems/P09-rje.pl
@@ -24,27 +24,21 @@ use v6;
 #   My first Perl6 script - I'm sure this can be done better
 #
 my @in = <a a a a b c c a a d e e e e>;
-
 my @out = p09( @in );
-
 @out.perl.say;
-
 
 #
 #  Here's where the work is done.
 #
-sub p09
-{
-   my @in = @_;
-   my @out = [ @in.shift ];
+sub p09 {
+    my @in = @_;
+    my @out = [ @in.shift ];
 
-
-   for @in -> $elem
-   {
-      push @out, [] if $elem ne @out[*-1][0];
-      push @out[*-1], $elem;
-   }
-   return @out;
+    for @in -> $elem {
+        push @out, [] if $elem ne @out[*-1][0];
+        push @out[*-1], $elem;
+    }
+    return @out;
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
