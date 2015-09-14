72ceb8d861acd5869a4092ec7ab4245581de369f
Steve Mynott <steve.mynott@gmail.com>
2015-09-14 17:26:42 +0100

    revert debugging commit :/

--- htmlify.pl
+++ htmlify.pl
@@ -6,20 +6,20 @@ use Perl6::Examples;
 
 sub MAIN(:$no-highlight = False) {
     my %base-categories-table =
-    #        "best-of-rosettacode" => "Best of Rosettacode",
-    #        "99-problems"         => "99 problems",
+            "best-of-rosettacode" => "Best of Rosettacode",
+            "99-problems"         => "99 problems",
             "cookbook"            => "Cookbook examples",
-    #        "euler"               => "Answers for Project Euler",
-    #        "games"               => "Games written in Perl 6",
-    #        "interpreters"        => "Language or DSL interpreters",
-    #        "module-management"   => "Module management",
-    #        "parsers"             => "Example grammars",
-    #        "perlmonks"           => "Answers to perlmonks.org questions",
-    #        "rosalind"            => "Bioinformatics programming problems",
-    #        "shootout"            => "The Computer Language Benchmark Game",
-    #        "tutorial"            => "Tutorial examples",
-    #        "wsg"                 => "The Winter Scripting Games",
-    #        "other"               => "Uncategorized examples",
+            "euler"               => "Answers for Project Euler",
+            "games"               => "Games written in Perl 6",
+            "interpreters"        => "Language or DSL interpreters",
+            "module-management"   => "Module management",
+            "parsers"             => "Example grammars",
+            "perlmonks"           => "Answers to perlmonks.org questions",
+            "rosalind"            => "Bioinformatics programming problems",
+            "shootout"            => "The Computer Language Benchmark Game",
+            "tutorial"            => "Tutorial examples",
+            "wsg"                 => "The Winter Scripting Games",
+            "other"               => "Uncategorized examples",
     ;
 
     my %menu-tabs =
