c12b95d49608bc21810627901c9e6416ab85bbbe
Steve Mynott <steve.mynott@gmail.com>
2015-09-15 14:58:44 +0100

    [cookbox] .Str now needed for <1 2>

--- categories/cookbook/04arrays/04-05iterating-over-an-array.pl
+++ categories/cookbook/04arrays/04-05iterating-over-an-array.pl
@@ -13,9 +13,9 @@ You want to iterate over the elements in an array
 my @a = <94 13 97 95 12 13 74 10 47 4 62 47 75 36 25 35 0 71 56 50 72 39 30 93>;
 
 for @a -> $e {
-    say $e;
+    say $e.Str;
 }
 
-say $_ for @a;
+say $_.Str for @a;
 
 # vim: expandtab shiftwidth=4 ft=perl6
