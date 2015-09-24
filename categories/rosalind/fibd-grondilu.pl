2a8b315b09d8c76601443447c6fcfb0bd07c0953
Steve Mynott <steve.mynott@gmail.com>
2015-09-24 14:52:20 +0000

    [rosalind] flatten the Fibonacci Rabbits

--- categories/rosalind/fibd-grondilu.pl
+++ categories/rosalind/fibd-grondilu.pl
@@ -19,7 +19,7 @@ Sample outut
 =end pod
 
 sub MAIN(Int $n = 6, Int $m = 3) {
-    my @population = 1, 0 xx ($m-1);
+    my @population = (1, 0 xx ($m-1)).flat;
     for 1 .. $n-1 -> \n {
         @population.unshift: [+] @population[1..*-1];  # reproduction
         @population.pop;                               # death
