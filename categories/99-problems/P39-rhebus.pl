033e62dcfcda7ed2af36da2c6126215fa7399756
Ovid <curtis_ovid_poe@yahoo.com>
2016-04-12 22:54:34 +0200

    Make this more perl6ish.
    Also removed the last example because it was basically a broken duplicate of
    the first example

--- categories/99-problems/P39-rhebus.pl
+++ categories/99-problems/P39-rhebus.pl
@@ -4,7 +4,7 @@ use v6;
 
 =TITLE P39 - A list of prime numbers.
 
-=AUTHOR Philip Potter
+=AUTHOR Curtis "Ovid" Poe
 
 =head1 Specification
 
@@ -14,34 +14,15 @@ use v6;
 
 =head1 Example
 
-    > say ~primes(10..20);
+    > say ~ grep { .is-prime }, 10..19;
     11 13 17 19
 
 =end pod
 
-# Copied from P31-rhebus.pl
-sub is_prime (Int $n) {
-    for 2..sqrt $n -> $k {
-        return Bool::False if $n %% $k;
-    }
-    return Bool::True;
-}
-
-
-# *@range is a slurpy parameter - it will swallow all the arguments passed
-sub primes (*@range) {
-    gather for @range {
-        take $_ if is_prime $_;
-    }
-}
-
 # we can call it with a range, as in the specification...
-say ~primes(10..20);
+say ~ grep { .is-prime }, 10..20;
 
 # or we can pass a list...
-say ~primes(3,5,17,257,65537);
-
-# or a series...
-say ~primes(1,2,*+1...100);
+say ~ grep { .is-prime }, 3,5,17,257,65537;
 
 # vim: expandtab shiftwidth=4 ft=perl6
